da09b65e02c371f36f32f30216bdb6bf8adf4745
PedroMarthon <pedro_marthon@live.com>
2018-03-28 23:11:11 +0300

    INIT

---
+++ SafeBoxes_TS2_ALAFAK/Frm_main.vb
@@ -0,0 +1,9 @@
+﻿Public Class Frm_main
+    Private Sub Frm_main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim ds As DataSet
+        ds = ReadQueryOut("SELECT * FROM TestTable")
+        MsgBox(ds.Tables(0).Rows.Count)
+        ds = ReadQueryOut("SELECT * FROM TestTable")
+        MsgBox(ds.Tables(0).Rows(0).Item(1))
+    End Sub
+End Class
---
+++ SafeBoxes_TS2_ALAFAK/OleDb_Tools.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data.OleDb
+Module OleDb_Tools
+    Private theCon As New OleDbConnection
+    Private conString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DATADIRECTORY|testDatabase.accdb;"
+    Private theCommand As New OleDbCommand
+    Private dbDataAdapter As New OleDbDataAdapter
+    Private dbDataSet As New DataSet
+    Private dbDataTable As New DataTable
+
+    Public Sub InitCon()
+        theCon.ConnectionString = conString
+        theCon.Open()
+    End Sub
+    Public Function ReadQueryOut(ByVal theQuery As String) As DataSet
+        theCon.ConnectionString = conString
+        theCommand.CommandText = theQuery
+        theCommand.Connection = theCon
+        dbDataAdapter.SelectCommand = theCommand
+        dbDataAdapter.Fill(dbDataSet)
+        dbDataAdapter.Dispose()
+        Return dbDataSet
+
+    End Function
+
+End Module
