d91b6d8edded1dd50f017578d1f3e059426cbe2e
PedroMarthon <pedro_marthon@live.com>
2018-03-28 23:56:40 +0300

    Commented Codes

--- SafeBoxes_TS2_ALAFAK/Frm_main.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_main.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Frm_main
     Private Sub Frm_main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        InitCon()
         Dim ds As DataSet
         ds = ReadQueryOut("SELECT * FROM TestTable")
         MsgBox(ds.Tables(0).Rows.Count)
--- SafeBoxes_TS2_ALAFAK/OleDb_Tools.vb
+++ SafeBoxes_TS2_ALAFAK/OleDb_Tools.vb
@@ -1,25 +1,37 @@
 ﻿Imports System.Data.OleDb
 Module OleDb_Tools
-    Private theCon As New OleDbConnection
+    'Declare the connection string to be used later (change this when LDM database is done)
     Private conString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DATADIRECTORY|testDatabase.accdb;"
-    Private theCommand As New OleDbCommand
-    Private dbDataAdapter As New OleDbDataAdapter
-    Private dbDataSet As New DataSet
-    Private dbDataTable As New DataTable
 
+    'Declare the connection using the connection string
+    Private theCon As New OleDbConnection(conString)
+
+    'Declare the command that will be executed as query
+    Private theCommand As New OleDbCommand
+
+    'Declare Data Adapter that will be used to get data tables from the database
+    Private dbDataAdapter As New OleDbDataAdapter
+
+    'Declare Data Set that will hold the data that is given by the Data Table
+    Private dbDataSet As New DataSet
+
+    'Used to initialize the connection at the main form load
     Public Sub InitCon()
-        theCon.ConnectionString = conString
         theCon.Open()
     End Sub
-    Public Function ReadQueryOut(ByVal theQuery As String) As DataSet
-        theCon.ConnectionString = conString
-        theCommand.CommandText = theQuery
-        theCommand.Connection = theCon
-        dbDataAdapter.SelectCommand = theCommand
-        dbDataAdapter.Fill(dbDataSet)
-        dbDataAdapter.Dispose()
-        Return dbDataSet
 
+    'This will be used with queries that has output tables like SELECT...
+    'Mostly SELECT...
+    '...Only SELECT :P
+    Public Function ReadQueryOut(ByVal theQuery As String) As DataSet
+
+        theCommand.CommandText = theQuery 'Set the command text (set the query)
+        theCommand.Connection = theCon 'Set the connection the will be used by the command
+        dbDataAdapter.SelectCommand = theCommand 'Append the command to the data adapter and execute
+        dbDataAdapter.Fill(dbDataSet) 'Fill the data from the data adapter to the data set to be readed
+        dbDataAdapter.Dispose() 'Distroy the data adapter to conserve memory since it is not needed anymore
+        Return dbDataSet 'Return the data set to be readed by the caller
     End Function
+
 
 End Module
