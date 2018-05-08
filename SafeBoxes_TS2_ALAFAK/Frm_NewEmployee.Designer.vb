288aa34593ed552764b4d401f08ab344101bf669
Hadi Deeb <kadu.coolboy@live.com>
2018-05-09 00:14:13 +0300

    push this one
    please run and test the code cause i cant debug because of crystal report errors :/

--- SafeBoxes_TS2_ALAFAK/Frm_NewEmployee.Designer.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_NewEmployee.Designer.vb
@@ -38,7 +38,7 @@ Partial Class Frm_NewEmployee
         Me.MaterialLabel7.Depth = 0
         Me.MaterialLabel7.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel7.Location = New System.Drawing.Point(14, 162)
+        Me.MaterialLabel7.Location = New System.Drawing.Point(28, 153)
         Me.MaterialLabel7.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.MaterialLabel7.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel7.Name = "MaterialLabel7"
@@ -48,18 +48,18 @@ Partial Class Frm_NewEmployee
         '
         'txt_lname
         '
-        Me.txt_lname.Location = New System.Drawing.Point(18, 186)
+        Me.txt_lname.Location = New System.Drawing.Point(32, 177)
         Me.txt_lname.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txt_lname.Name = "txt_lname"
-        Me.txt_lname.Size = New System.Drawing.Size(198, 20)
+        Me.txt_lname.Size = New System.Drawing.Size(180, 20)
         Me.txt_lname.TabIndex = 54
         '
         'txt_father
         '
-        Me.txt_father.Location = New System.Drawing.Point(18, 128)
+        Me.txt_father.Location = New System.Drawing.Point(32, 128)
         Me.txt_father.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txt_father.Name = "txt_father"
-        Me.txt_father.Size = New System.Drawing.Size(198, 20)
+        Me.txt_father.Size = New System.Drawing.Size(180, 20)
         Me.txt_father.TabIndex = 53
         '
         'MaterialLabel6
@@ -68,7 +68,7 @@ Partial Class Frm_NewEmployee
         Me.MaterialLabel6.Depth = 0
         Me.MaterialLabel6.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel6.Location = New System.Drawing.Point(14, 104)
+        Me.MaterialLabel6.Location = New System.Drawing.Point(28, 104)
         Me.MaterialLabel6.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.MaterialLabel6.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel6.Name = "MaterialLabel6"
@@ -82,7 +82,7 @@ Partial Class Frm_NewEmployee
         Me.MaterialLabel5.Depth = 0
         Me.MaterialLabel5.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel5.Location = New System.Drawing.Point(14, 49)
+        Me.MaterialLabel5.Location = New System.Drawing.Point(28, 49)
         Me.MaterialLabel5.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.MaterialLabel5.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel5.Name = "MaterialLabel5"
@@ -92,7 +92,7 @@ Partial Class Frm_NewEmployee
         '
         'btn_submit
         '
-        Me.btn_submit.Location = New System.Drawing.Point(144, 240)
+        Me.btn_submit.Location = New System.Drawing.Point(134, 212)
         Me.btn_submit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btn_submit.Name = "btn_submit"
         Me.btn_submit.Size = New System.Drawing.Size(112, 35)
@@ -102,10 +102,10 @@ Partial Class Frm_NewEmployee
         '
         'txt_fname
         '
-        Me.txt_fname.Location = New System.Drawing.Point(18, 73)
+        Me.txt_fname.Location = New System.Drawing.Point(32, 73)
         Me.txt_fname.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txt_fname.Name = "txt_fname"
-        Me.txt_fname.Size = New System.Drawing.Size(198, 20)
+        Me.txt_fname.Size = New System.Drawing.Size(180, 20)
         Me.txt_fname.TabIndex = 49
         '
         'lbl_Empid
@@ -114,19 +114,19 @@ Partial Class Frm_NewEmployee
         Me.lbl_Empid.Depth = 0
         Me.lbl_Empid.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.lbl_Empid.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.lbl_Empid.Location = New System.Drawing.Point(14, 12)
+        Me.lbl_Empid.Location = New System.Drawing.Point(9, 9)
         Me.lbl_Empid.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.lbl_Empid.MouseState = MaterialSkin.MouseState.HOVER
         Me.lbl_Empid.Name = "lbl_Empid"
-        Me.lbl_Empid.Size = New System.Drawing.Size(195, 19)
+        Me.lbl_Empid.Size = New System.Drawing.Size(159, 19)
         Me.lbl_Empid.TabIndex = 48
-        Me.lbl_Empid.Text = "Employee ID: <EmployeeId>"
+        Me.lbl_Empid.Text = "Employee ID: <EmpId>"
         '
-        'Frm_NewEmployee
+        'frm_NewEmployee
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(269, 289)
+        Me.ClientSize = New System.Drawing.Size(259, 261)
         Me.Controls.Add(Me.MaterialLabel7)
         Me.Controls.Add(Me.txt_lname)
         Me.Controls.Add(Me.txt_father)
@@ -135,8 +135,8 @@ Partial Class Frm_NewEmployee
         Me.Controls.Add(Me.btn_submit)
         Me.Controls.Add(Me.txt_fname)
         Me.Controls.Add(Me.lbl_Empid)
-        Me.Name = "Frm_NewEmployee"
-        Me.Text = "Frm_NewEmployee"
+        Me.Name = "frm_NewEmployee"
+        Me.Text = "frm_NewEmployee"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
--- SafeBoxes_TS2_ALAFAK/Frm_NewEmployee.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_NewEmployee.vb
@@ -1,13 +1,7 @@
 ﻿Public Class Frm_NewEmployee
     Dim theNewId As Integer
     Private Sub btn_submit_Click(sender As Object, e As EventArgs) Handles btn_submit.Click
-        If txt_fname.Text = "" Or txt_lname.Text = "" Or txt_father.Text = "" Then
-            MessageBox.Show("Please fill all needed information!")
-        Else
-            ExecuteQuery("UPDATE Employees SET EmpFName = '" & txt_fname.Text & "', EmpLName = '" & txt_lname.Text & "', EmpMName = '" & txt_father.Text & " WHERE EmpId = " & theNewId)
-            Frm_main.clientid = theNewId
-            Me.Dispose()
-        End If
+
     End Sub
 
     Private Sub Frm_NewEmployee_Closed(sender As Object, e As EventArgs) Handles Me.Closed
@@ -23,12 +17,10 @@
 
     Private Sub txt_father_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_father.KeyPress
         Only_char(txt_father, e)
-
     End Sub
 
     Private Sub txt_fname_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_fname.KeyPress
         Only_char(txt_fname, e)
-
     End Sub
 
     Private Sub txt_lname_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_lname.KeyPress
