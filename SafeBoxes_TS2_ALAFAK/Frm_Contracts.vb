9d3576cc789e36227e08249fe1d6721d14409615
Pedro Marthon <pedro_marthon@live.com>
2018-05-09 22:34:34 +0300

    Added initial contract manager setup

--- SafeBoxes_TS2_ALAFAK/Frm_Contracts.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_Contracts.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Frm_Contracts
     Private Sub Frm_Contracts_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        FillDGV(dgv_contracts, "SELECT ContId AS [ID], ContBDate [Contract Date], ContToDate [Expire Date] BoxId, ContBDate FROM Contract")
+        FillDGV(dgv_contracts, "SELECT ContId AS ID, ContBDate AS [Contract Date], ContToDate AS [Expire Date], ContPhone1 AS [Phone1], ContPhone2 AS [Phone2], BoxId AS [Box ID], AccountId AS [Account ID], BuildingName AS Address, EmpFName + ' ' + EmpLName AS Employee FROM Contract, Buildings, Employees WHERE Contract.BuildingId = Buildings.BuildingId AND Contract.EmpId = Employees.EmpId")
     End Sub
 End Class
