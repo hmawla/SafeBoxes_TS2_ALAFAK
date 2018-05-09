bdc87000425a529e86413b23742a56ecd886052a
Pedro Marthon <pedro_marthon@live.com>
2018-05-09 09:15:08 +0300

    removed crystal report

--- SafeBoxes_TS2_ALAFAK/Frm_main.Designer.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_main.Designer.vb
@@ -26,8 +26,6 @@ Partial Class Frm_main
         Me.btn_newclient = New MaterialSkin.Controls.MaterialRaisedButton()
         Me.MaterialRaisedButton2 = New MaterialSkin.Controls.MaterialRaisedButton()
         Me.MaterialRaisedButton3 = New MaterialSkin.Controls.MaterialRaisedButton()
-        Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
-        Me.CrystalReport11 = New SafeBoxes_TS2_ALAFAK.CrystalReport1()
         Me.SuspendLayout()
         '
         'MaterialRaisedButton1
@@ -94,23 +92,11 @@ Partial Class Frm_main
         Me.MaterialRaisedButton3.Text = "New Info Vouch"
         Me.MaterialRaisedButton3.UseVisualStyleBackColor = True
         '
-        'CrystalReportViewer1
-        '
-        Me.CrystalReportViewer1.ActiveViewIndex = 0
-        Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
-        Me.CrystalReportViewer1.Location = New System.Drawing.Point(13, 117)
-        Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
-        Me.CrystalReportViewer1.ReportSource = Me.CrystalReport11
-        Me.CrystalReportViewer1.Size = New System.Drawing.Size(790, 372)
-        Me.CrystalReportViewer1.TabIndex = 56
-        '
         'Frm_main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(959, 501)
-        Me.Controls.Add(Me.CrystalReportViewer1)
         Me.Controls.Add(Me.MaterialRaisedButton3)
         Me.Controls.Add(Me.MaterialRaisedButton2)
         Me.Controls.Add(Me.btn_newclient)
@@ -128,6 +114,4 @@ Partial Class Frm_main
     Friend WithEvents btn_newclient As MaterialSkin.Controls.MaterialRaisedButton
     Friend WithEvents MaterialRaisedButton2 As MaterialSkin.Controls.MaterialRaisedButton
     Friend WithEvents MaterialRaisedButton3 As MaterialSkin.Controls.MaterialRaisedButton
-    Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
-    Friend WithEvents CrystalReport11 As CrystalReport1
 End Class
