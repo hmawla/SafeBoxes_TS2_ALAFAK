86468c8f012730c9065b37adae9d56e3d40a9dca
Pedro Marthon <pedro_marthon@live.com>
2018-05-09 21:54:30 +0300

    adding buttons to main form

--- SafeBoxes_TS2_ALAFAK/Frm_main.Designer.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_main.Designer.vb
@@ -27,6 +27,19 @@ Partial Class Frm_main
         Me.MaterialRaisedButton2 = New MaterialSkin.Controls.MaterialRaisedButton()
         Me.MaterialRaisedButton3 = New MaterialSkin.Controls.MaterialRaisedButton()
         Me.MaterialRaisedButton4 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton5 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton6 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton7 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton8 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton9 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton10 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton11 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton12 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton13 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton14 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton15 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton16 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton17 = New MaterialSkin.Controls.MaterialRaisedButton()
         Me.SuspendLayout()
         '
         'MaterialRaisedButton1
@@ -35,7 +48,7 @@ Partial Class Frm_main
         Me.MaterialRaisedButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.MaterialRaisedButton1.Depth = 0
         Me.MaterialRaisedButton1.Icon = Nothing
-        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(13, 73)
+        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(13, 14)
         Me.MaterialRaisedButton1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaterialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialRaisedButton1.Name = "MaterialRaisedButton1"
@@ -51,7 +64,7 @@ Partial Class Frm_main
         Me.btn_newclient.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.btn_newclient.Depth = 0
         Me.btn_newclient.Icon = Nothing
-        Me.btn_newclient.Location = New System.Drawing.Point(211, 73)
+        Me.btn_newclient.Location = New System.Drawing.Point(147, 14)
         Me.btn_newclient.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btn_newclient.MouseState = MaterialSkin.MouseState.HOVER
         Me.btn_newclient.Name = "btn_newclient"
@@ -67,7 +80,7 @@ Partial Class Frm_main
         Me.MaterialRaisedButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.MaterialRaisedButton2.Depth = 0
         Me.MaterialRaisedButton2.Icon = Nothing
-        Me.MaterialRaisedButton2.Location = New System.Drawing.Point(370, 73)
+        Me.MaterialRaisedButton2.Location = New System.Drawing.Point(255, 14)
         Me.MaterialRaisedButton2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaterialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialRaisedButton2.Name = "MaterialRaisedButton2"
@@ -83,7 +96,7 @@ Partial Class Frm_main
         Me.MaterialRaisedButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.MaterialRaisedButton3.Depth = 0
         Me.MaterialRaisedButton3.Icon = Nothing
-        Me.MaterialRaisedButton3.Location = New System.Drawing.Point(559, 73)
+        Me.MaterialRaisedButton3.Location = New System.Drawing.Point(383, 14)
         Me.MaterialRaisedButton3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaterialRaisedButton3.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialRaisedButton3.Name = "MaterialRaisedButton3"
@@ -99,7 +112,7 @@ Partial Class Frm_main
         Me.MaterialRaisedButton4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.MaterialRaisedButton4.Depth = 0
         Me.MaterialRaisedButton4.Icon = Nothing
-        Me.MaterialRaisedButton4.Location = New System.Drawing.Point(762, 73)
+        Me.MaterialRaisedButton4.Location = New System.Drawing.Point(525, 14)
         Me.MaterialRaisedButton4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaterialRaisedButton4.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialRaisedButton4.Name = "MaterialRaisedButton4"
@@ -109,11 +122,232 @@ Partial Class Frm_main
         Me.MaterialRaisedButton4.Text = "New Employee"
         Me.MaterialRaisedButton4.UseVisualStyleBackColor = True
         '
+        'MaterialRaisedButton5
+        '
+        Me.MaterialRaisedButton5.AutoSize = True
+        Me.MaterialRaisedButton5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton5.Depth = 0
+        Me.MaterialRaisedButton5.Icon = Nothing
+        Me.MaterialRaisedButton5.Location = New System.Drawing.Point(13, 60)
+        Me.MaterialRaisedButton5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton5.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton5.Name = "MaterialRaisedButton5"
+        Me.MaterialRaisedButton5.Primary = True
+        Me.MaterialRaisedButton5.Size = New System.Drawing.Size(113, 36)
+        Me.MaterialRaisedButton5.TabIndex = 6
+        Me.MaterialRaisedButton5.Text = "Missing Keys"
+        Me.MaterialRaisedButton5.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton6
+        '
+        Me.MaterialRaisedButton6.AutoSize = True
+        Me.MaterialRaisedButton6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton6.Depth = 0
+        Me.MaterialRaisedButton6.Icon = Nothing
+        Me.MaterialRaisedButton6.Location = New System.Drawing.Point(134, 60)
+        Me.MaterialRaisedButton6.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton6.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton6.Name = "MaterialRaisedButton6"
+        Me.MaterialRaisedButton6.Primary = True
+        Me.MaterialRaisedButton6.Size = New System.Drawing.Size(145, 36)
+        Me.MaterialRaisedButton6.TabIndex = 7
+        Me.MaterialRaisedButton6.Text = "Contract Ending"
+        Me.MaterialRaisedButton6.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton7
+        '
+        Me.MaterialRaisedButton7.AutoSize = True
+        Me.MaterialRaisedButton7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton7.Depth = 0
+        Me.MaterialRaisedButton7.Icon = Nothing
+        Me.MaterialRaisedButton7.Location = New System.Drawing.Point(287, 60)
+        Me.MaterialRaisedButton7.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton7.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton7.Name = "MaterialRaisedButton7"
+        Me.MaterialRaisedButton7.Primary = True
+        Me.MaterialRaisedButton7.Size = New System.Drawing.Size(207, 36)
+        Me.MaterialRaisedButton7.TabIndex = 8
+        Me.MaterialRaisedButton7.Text = "Contract Bill permission"
+        Me.MaterialRaisedButton7.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton8
+        '
+        Me.MaterialRaisedButton8.AutoSize = True
+        Me.MaterialRaisedButton8.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton8.Depth = 0
+        Me.MaterialRaisedButton8.Icon = Nothing
+        Me.MaterialRaisedButton8.Location = New System.Drawing.Point(502, 60)
+        Me.MaterialRaisedButton8.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton8.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton8.Name = "MaterialRaisedButton8"
+        Me.MaterialRaisedButton8.Primary = True
+        Me.MaterialRaisedButton8.Size = New System.Drawing.Size(142, 36)
+        Me.MaterialRaisedButton8.TabIndex = 9
+        Me.MaterialRaisedButton8.Text = "Contract Renew"
+        Me.MaterialRaisedButton8.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton9
+        '
+        Me.MaterialRaisedButton9.AutoSize = True
+        Me.MaterialRaisedButton9.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton9.Depth = 0
+        Me.MaterialRaisedButton9.Icon = Nothing
+        Me.MaterialRaisedButton9.Location = New System.Drawing.Point(13, 106)
+        Me.MaterialRaisedButton9.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton9.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton9.Name = "MaterialRaisedButton9"
+        Me.MaterialRaisedButton9.Primary = True
+        Me.MaterialRaisedButton9.Size = New System.Drawing.Size(121, 36)
+        Me.MaterialRaisedButton9.TabIndex = 10
+        Me.MaterialRaisedButton9.Text = "New SignCard"
+        Me.MaterialRaisedButton9.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton10
+        '
+        Me.MaterialRaisedButton10.AutoSize = True
+        Me.MaterialRaisedButton10.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton10.Depth = 0
+        Me.MaterialRaisedButton10.Icon = Nothing
+        Me.MaterialRaisedButton10.Location = New System.Drawing.Point(142, 106)
+        Me.MaterialRaisedButton10.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton10.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton10.Name = "MaterialRaisedButton10"
+        Me.MaterialRaisedButton10.Primary = True
+        Me.MaterialRaisedButton10.Size = New System.Drawing.Size(128, 36)
+        Me.MaterialRaisedButton10.TabIndex = 11
+        Me.MaterialRaisedButton10.Text = "New Signature"
+        Me.MaterialRaisedButton10.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton11
+        '
+        Me.MaterialRaisedButton11.AutoSize = True
+        Me.MaterialRaisedButton11.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton11.Depth = 0
+        Me.MaterialRaisedButton11.Icon = Nothing
+        Me.MaterialRaisedButton11.Location = New System.Drawing.Point(278, 106)
+        Me.MaterialRaisedButton11.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton11.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton11.Name = "MaterialRaisedButton11"
+        Me.MaterialRaisedButton11.Primary = True
+        Me.MaterialRaisedButton11.Size = New System.Drawing.Size(120, 36)
+        Me.MaterialRaisedButton11.TabIndex = 12
+        Me.MaterialRaisedButton11.Text = "Nationalities"
+        Me.MaterialRaisedButton11.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton12
+        '
+        Me.MaterialRaisedButton12.AutoSize = True
+        Me.MaterialRaisedButton12.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton12.Depth = 0
+        Me.MaterialRaisedButton12.Icon = Nothing
+        Me.MaterialRaisedButton12.Location = New System.Drawing.Point(406, 106)
+        Me.MaterialRaisedButton12.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton12.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton12.Name = "MaterialRaisedButton12"
+        Me.MaterialRaisedButton12.Primary = True
+        Me.MaterialRaisedButton12.Size = New System.Drawing.Size(64, 36)
+        Me.MaterialRaisedButton12.TabIndex = 13
+        Me.MaterialRaisedButton12.Text = "FATCA"
+        Me.MaterialRaisedButton12.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton13
+        '
+        Me.MaterialRaisedButton13.AutoSize = True
+        Me.MaterialRaisedButton13.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton13.Depth = 0
+        Me.MaterialRaisedButton13.Icon = Nothing
+        Me.MaterialRaisedButton13.Location = New System.Drawing.Point(478, 106)
+        Me.MaterialRaisedButton13.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton13.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton13.Name = "MaterialRaisedButton13"
+        Me.MaterialRaisedButton13.Primary = True
+        Me.MaterialRaisedButton13.Size = New System.Drawing.Size(128, 36)
+        Me.MaterialRaisedButton13.TabIndex = 14
+        Me.MaterialRaisedButton13.Text = "Authorization"
+        Me.MaterialRaisedButton13.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton14
+        '
+        Me.MaterialRaisedButton14.AutoSize = True
+        Me.MaterialRaisedButton14.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton14.Depth = 0
+        Me.MaterialRaisedButton14.Icon = Nothing
+        Me.MaterialRaisedButton14.Location = New System.Drawing.Point(13, 152)
+        Me.MaterialRaisedButton14.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton14.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton14.Name = "MaterialRaisedButton14"
+        Me.MaterialRaisedButton14.Primary = True
+        Me.MaterialRaisedButton14.Size = New System.Drawing.Size(104, 36)
+        Me.MaterialRaisedButton14.TabIndex = 15
+        Me.MaterialRaisedButton14.Text = "Info Result"
+        Me.MaterialRaisedButton14.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton15
+        '
+        Me.MaterialRaisedButton15.AutoSize = True
+        Me.MaterialRaisedButton15.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton15.Depth = 0
+        Me.MaterialRaisedButton15.Icon = Nothing
+        Me.MaterialRaisedButton15.Location = New System.Drawing.Point(125, 152)
+        Me.MaterialRaisedButton15.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton15.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton15.Name = "MaterialRaisedButton15"
+        Me.MaterialRaisedButton15.Primary = True
+        Me.MaterialRaisedButton15.Size = New System.Drawing.Size(92, 36)
+        Me.MaterialRaisedButton15.TabIndex = 16
+        Me.MaterialRaisedButton15.Text = "Connects"
+        Me.MaterialRaisedButton15.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton16
+        '
+        Me.MaterialRaisedButton16.AutoSize = True
+        Me.MaterialRaisedButton16.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton16.Depth = 0
+        Me.MaterialRaisedButton16.Icon = Nothing
+        Me.MaterialRaisedButton16.Location = New System.Drawing.Point(225, 152)
+        Me.MaterialRaisedButton16.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton16.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton16.Name = "MaterialRaisedButton16"
+        Me.MaterialRaisedButton16.Primary = True
+        Me.MaterialRaisedButton16.Size = New System.Drawing.Size(62, 36)
+        Me.MaterialRaisedButton16.TabIndex = 17
+        Me.MaterialRaisedButton16.Text = "Visits"
+        Me.MaterialRaisedButton16.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton17
+        '
+        Me.MaterialRaisedButton17.AutoSize = True
+        Me.MaterialRaisedButton17.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton17.Depth = 0
+        Me.MaterialRaisedButton17.Icon = Nothing
+        Me.MaterialRaisedButton17.Location = New System.Drawing.Point(295, 152)
+        Me.MaterialRaisedButton17.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaterialRaisedButton17.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton17.Name = "MaterialRaisedButton17"
+        Me.MaterialRaisedButton17.Primary = True
+        Me.MaterialRaisedButton17.Size = New System.Drawing.Size(80, 36)
+        Me.MaterialRaisedButton17.TabIndex = 18
+        Me.MaterialRaisedButton17.Text = "Careers"
+        Me.MaterialRaisedButton17.UseVisualStyleBackColor = True
+        '
         'Frm_main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(959, 501)
+        Me.ClientSize = New System.Drawing.Size(679, 221)
+        Me.Controls.Add(Me.MaterialRaisedButton17)
+        Me.Controls.Add(Me.MaterialRaisedButton16)
+        Me.Controls.Add(Me.MaterialRaisedButton15)
+        Me.Controls.Add(Me.MaterialRaisedButton14)
+        Me.Controls.Add(Me.MaterialRaisedButton13)
+        Me.Controls.Add(Me.MaterialRaisedButton12)
+        Me.Controls.Add(Me.MaterialRaisedButton11)
+        Me.Controls.Add(Me.MaterialRaisedButton10)
+        Me.Controls.Add(Me.MaterialRaisedButton9)
+        Me.Controls.Add(Me.MaterialRaisedButton8)
+        Me.Controls.Add(Me.MaterialRaisedButton7)
+        Me.Controls.Add(Me.MaterialRaisedButton6)
+        Me.Controls.Add(Me.MaterialRaisedButton5)
         Me.Controls.Add(Me.MaterialRaisedButton4)
         Me.Controls.Add(Me.MaterialRaisedButton3)
         Me.Controls.Add(Me.MaterialRaisedButton2)
@@ -133,4 +367,17 @@ Partial Class Frm_main
     Friend WithEvents MaterialRaisedButton2 As MaterialSkin.Controls.MaterialRaisedButton
     Friend WithEvents MaterialRaisedButton3 As MaterialSkin.Controls.MaterialRaisedButton
     Friend WithEvents MaterialRaisedButton4 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton5 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton6 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton7 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton8 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton9 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton10 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton11 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton12 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton13 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton14 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton15 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton16 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton17 As MaterialSkin.Controls.MaterialRaisedButton
 End Class
