1398cfc7a89e911b643a5b360501cf5abe2623e7
Hadi Deeb <kadu.coolboy@live.com>
2018-05-10 14:01:29 +0300

    working on clients
    still have some errors we need to fix :banana:

--- SafeBoxes_TS2_ALAFAK/Frm_Clients.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_Clients.vb
@@ -1,5 +1,36 @@
-﻿Public Class Frm_Clients
+﻿
+Public Class Frm_Clients
+    Public clientId As Integer
     Private Sub Frm_Clients_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         FillDGV(dgv_clients, "SELECT ClientId AS [ID], ClientFName AS [First Name], ClientMName AS [Father Name], ClientLName AS [Last Name], ClientDOB AS [Date Of Birth], ClientMother AS [Mother Name], ClientRegisterNbr, PostBoxNbr,RegionName  FROM Clients c,Regions r WHERE c.RegionId=r.RegionId")
+    End Sub
+
+    Private Sub btn_newclient_Click(sender As Object, e As EventArgs) Handles btn_newclient.Click
+        clientId = 0
+        Frm_NewClient.ShowDialog()
+        FillDGV(dgv_clients, "SELECT ClientId AS [ID], ClientFName AS [First Name], ClientMName AS [Father Name], ClientLName AS [Last Name], ClientDOB AS [Date Of Birth], ClientMother AS [Mother Name], ClientRegisterNbr, PostBoxNbr,RegionName  FROM Clients c,Regions r WHERE c.RegionId=r.RegionId")
+    End Sub
+
+    Private Sub btn_modclient_Click(sender As Object, e As EventArgs) Handles btn_modclient.Click
+        clientId = dgv_clients.SelectedRows(0).Cells(0).Value
+        Frm_NewClient.ShowDialog()
+        FillDGV(dgv_clients, "SELECT ClientId AS [ID], ClientFName AS [First Name], ClientMName AS [Father Name], ClientLName AS [Last Name], ClientDOB AS [Date Of Birth], ClientMother AS [Mother Name], ClientRegisterNbr, PostBoxNbr,RegionName  FROM Clients c,Regions r WHERE c.RegionId=r.RegionId")
+    End Sub
+
+    Private Sub btn_delclient_Click(sender As Object, e As EventArgs) Handles btn_delclient.Click
+        ExecuteQuery("DELETE FROM Clients WHERE ClientId = " & dgv_clients.SelectedRows(0).Cells(0).Value)
+        FillDGV(dgv_clients, "SELECT ClientId AS [ID], ClientFName AS [First Name], ClientMName AS [Father Name], ClientLName AS [Last Name], ClientDOB AS [Date Of Birth], ClientMother AS [Mother Name], ClientRegisterNbr, PostBoxNbr,RegionName  FROM Clients c,Regions r WHERE c.RegionId=r.RegionId")
+    End Sub
+
+    Private Sub dgv_clients_SelectionChanged(sender As Object, e As EventArgs) Handles dgv_clients.SelectionChanged
+        Try
+            If dgv_clients.SelectedRows(0).Cells(0).Value > 0 Then
+            End If
+            btn_delclient.Enabled = True
+            btn_modclient.Enabled = True
+        Catch ex As Exception
+            btn_delclient.Enabled = False
+            btn_modclient.Enabled = False
+        End Try
     End Sub
 End Class
