a12113d9b85285e503799c1a50e4be1c3c3986c6
Hadi Deeb <kadu.coolboy@live.com>
2018-05-10 23:23:37 +0300

    companies addnew error fixed
    fuck u test ur code before releasing it :*

--- SafeBoxes_TS2_ALAFAK/Frm_Companies.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_Companies.vb
@@ -1,5 +1,38 @@
 ﻿Public Class Frm_Companies
+    Public companyId As Integer
     Private Sub Frm_Companies_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         FillDGV(dgv_companies, "SELECT CompId AS [ID], CompName AS [Company Name], CompType AS [Company Type]  FROM Company c,CompType ct WHERE c.CompTypeId=ct.CompTypeId")
+    End Sub
+
+    Private Sub btn_modcompany_Click(sender As Object, e As EventArgs) Handles btn_modcompany.Click
+        companyId = dgv_companies.SelectedRows(0).Cells(0).Value
+        Frm_NewCompany.ShowDialog()
+        FillDGV(dgv_companies, "SELECT CompId AS [ID], CompName AS [Company Name], CompType AS [Company Type]  FROM Company c,CompType ct WHERE c.CompTypeId=ct.CompTypeId")
+
+    End Sub
+
+    Private Sub btn_newcompany_Click(sender As Object, e As EventArgs) Handles btn_newcompany.Click
+        companyId = 0
+        Frm_NewCompany.ShowDialog()
+        FillDGV(dgv_companies, "SELECT CompId AS [ID], CompName AS [Company Name], CompType AS [Company Type]  FROM Company c,CompType ct WHERE c.CompTypeId=ct.CompTypeId")
+
+    End Sub
+
+    Private Sub btn_delcompany_Click(sender As Object, e As EventArgs) Handles btn_delcompany.Click
+        ExecuteQuery("DELETE FROM Company WHERE CompId = " & dgv_companies.SelectedRows(0).Cells(0).Value)
+        FillDGV(dgv_companies, "SELECT CompId AS [ID], CompName AS [Company Name], CompType AS [Company Type]  FROM Company c,CompType ct WHERE c.CompTypeId=ct.CompTypeId")
+
+    End Sub
+
+    Private Sub dgv_companies_SelectionChanged(sender As Object, e As EventArgs) Handles dgv_companies.SelectionChanged
+        Try
+            If dgv_companies.SelectedRows(0).Cells(0).Value > 0 Then
+            End If
+            btn_delcompany.Enabled = True
+            btn_modcompany.Enabled = True
+        Catch ex As Exception
+            btn_delcompany.Enabled = False
+            btn_modcompany.Enabled = False
+        End Try
     End Sub
 End Class
--- SafeBoxes_TS2_ALAFAK/Frm_NewCompany.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_NewCompany.vb
@@ -1,18 +1,35 @@
 ﻿Public Class Frm_NewCompany
     Dim theNewId As Integer
+    Dim isSubmitting As Boolean = False
 
     Private Sub Frm_NewCompany_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         FillCBox(cbox_comptypes, "SELECT CompTypeId, CompType FROM CompType", "CompTypeId", "CompType")
-        theNewId = genID("Company", "CompId")
-        ExecuteQuery("INSERT INTO Company(CompId) VALUES(" & theNewId & ")")
-        lbl_compid.Text = "Company ID: " & theNewId
+        If Frm_Companies.companyId = 0 Then
+            theNewId = genID("Company", "CompId")
+            ExecuteQuery("INSERT INTO Company(CompId) VALUES(" & theNewId & ")")
+            lbl_compid.Text = "Company ID: " & theNewId
+        Else
+            theNewId = Frm_Companies.companyId
+            lbl_compid.Text = "Company ID: " & theNewId
+            Dim theContDetails As New DataSet
+            theContDetails = ReadQueryOut("SELECT * FROM Company WHERE CompId = " & theNewId)
+            Dim rows As DataRow = theContDetails.Tables(0).Rows(0)
+            txt_compname.Text = rows.Item(1)
+            Dim theType As String = rows.Item(2)
+            theContDetails.Reset()
+            theContDetails = ReadQueryOut("SELECT CompType, CompTypeId FROM CompType WHERE CompTypeId = " & theType)
+            rows = theContDetails.Tables(0).Rows(0)
+            cbox_comptypes.SelectedValue = rows.Item(1)
+        End If
+
     End Sub
 
     Private Sub btn_submit_Click(sender As Object, e As EventArgs) Handles btn_submit.Click
+        isSubmitting = True
         If txt_compname.Text = "" Or cbox_comptypes.Text = "" Then
             MessageBox.Show("Fill all needed information!")
         Else
-            ExecuteQuery("UPDATE Company SET CompName = '" & txt_compname.Text & "', CompType = " & cbox_comptypes.SelectedValue & "WHERE CompId = " & theNewId)
+            ExecuteQuery("UPDATE Company SET CompName = '" & txt_compname.Text & "', CompTypeId = " & cbox_comptypes.SelectedValue & " WHERE CompId = " & theNewId)
             MessageBox.Show("Company Added!")
             Me.Close()
         End If
@@ -26,5 +43,12 @@
 
     Private Sub cbox_comptypes_KeyPress(sender As Object, e As KeyPressEventArgs) Handles cbox_comptypes.KeyPress
         Only_char(cbox_comptypes, e)
+    End Sub
+
+    Private Sub Frm_NewCompany_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        If Not isSubmitting Then
+            ExecuteQuery("DELETE FROM Company WHERE CompId = " & theNewId)
+            Me.Dispose()
+        End If
     End Sub
 End Class
