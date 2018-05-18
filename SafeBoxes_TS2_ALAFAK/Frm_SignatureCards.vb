be007644fdc69aa3767df9c7b4f403e23c94a13b
Pedro Marthon <pedro_marthon@live.com>
2018-05-18 15:29:32 +0300

    fixed code to see the problem

--- SafeBoxes_TS2_ALAFAK/Frm_SignatureCards.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_SignatureCards.vb
@@ -1,5 +1,11 @@
 ﻿Public Class Frm_SignatureCards
     Private Sub Frm_SignatureCards_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        FillDGV(dgv_signcard, "SELECT SignCardId AS [ID], SignCardDate AS [DATE], SignCardNote AS [NOTE], ClientFName AS [CLIENT NAME],Nationality,ContId,CareerName AS [CAREER],BuildingName FROM SignatureCards sc,Clients c,Nationalities nat,Careers ca,Buildings b WHERE sc.ClientId=c.CLientId AND sc.NationId=nat.NationId AND sc.Careerid=ca.Careerid AND sc.BuildingId=b.BuildingId")
+        FillDGV(dgv_signcard, "SELECT SignCardId AS [ID], SignCardDate AS [DATE],
+                                SignCardNote AS [NOTE], ClientFName AS [CLIENT NAME],
+                                Nationality, ContId, CareerName AS [CAREER], BuildingName 
+                                FROM SignatureCards sc, Clients c,
+                                Nationalities nat ,Careers ca ,Buildings b 
+                                WHERE sc.ClientId=c.CLientId AND sc.NationId=nat.NationId 
+                                AND sc.Careerid=ca.Careerid AND sc.BuildingId=b.BuildingId")
     End Sub
 End Class
