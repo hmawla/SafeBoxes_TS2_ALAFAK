fcbd4840359e0e722e1981ab6ae1af3d11e45f50
Hadi Deeb <kadu.coolboy@live.com>
2018-05-18 22:52:34 +0300

    hopaa crystal report fixxnxed

---
+++ SafeBoxes_TS2_ALAFAK/Frm_Report_Contract.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Frm_Report_Contract
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Txt_ContId = New System.Windows.Forms.TextBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.dtpick_exdate = New System.Windows.Forms.DateTimePicker()
+        Me.Rdb_ByContId = New System.Windows.Forms.RadioButton()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.Txt_ContId)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.dtpick_exdate)
+        Me.GroupBox1.Controls.Add(Me.Rdb_ByContId)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(688, 134)
+        Me.GroupBox1.TabIndex = 20
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Filter"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(579, 104)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(71, 17)
+        Me.CheckBox1.TabIndex = 17
+        Me.CheckBox1.Text = "Delivered"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Txt_ContId
+        '
+        Me.Txt_ContId.Location = New System.Drawing.Point(6, 57)
+        Me.Txt_ContId.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Txt_ContId.Name = "Txt_ContId"
+        Me.Txt_ContId.Size = New System.Drawing.Size(234, 20)
+        Me.Txt_ContId.TabIndex = 12
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(250, 25)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(121, 17)
+        Me.RadioButton2.TabIndex = 15
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "By Date (FROM-TO)"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'dtpick_exdate
+        '
+        Me.dtpick_exdate.CustomFormat = "dd/mm/yyyy"
+        Me.dtpick_exdate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.dtpick_exdate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpick_exdate.Location = New System.Drawing.Point(466, 55)
+        Me.dtpick_exdate.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.dtpick_exdate.MinDate = New Date(2018, 5, 4, 0, 0, 0, 0)
+        Me.dtpick_exdate.Name = "dtpick_exdate"
+        Me.dtpick_exdate.Size = New System.Drawing.Size(204, 26)
+        Me.dtpick_exdate.TabIndex = 10
+        '
+        'Rdb_ByContId
+        '
+        Me.Rdb_ByContId.AutoSize = True
+        Me.Rdb_ByContId.Location = New System.Drawing.Point(6, 25)
+        Me.Rdb_ByContId.Name = "Rdb_ByContId"
+        Me.Rdb_ByContId.Size = New System.Drawing.Size(94, 17)
+        Me.Rdb_ByContId.TabIndex = 16
+        Me.Rdb_ByContId.TabStop = True
+        Me.Rdb_ByContId.Text = "By Contract ID"
+        Me.Rdb_ByContId.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "dd/mm/yyyy"
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(250, 55)
+        Me.DateTimePicker1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.DateTimePicker1.MinDate = New Date(2018, 5, 4, 0, 0, 0, 0)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(204, 26)
+        Me.DateTimePicker1.TabIndex = 11
+        '
+        'CrystalReportViewer1
+        '
+        Me.CrystalReportViewer1.ActiveViewIndex = -1
+        Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CrystalReportViewer1.Location = New System.Drawing.Point(12, 152)
+        Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(688, 428)
+        Me.CrystalReportViewer1.TabIndex = 21
+        '
+        'Frm_Report_Contract
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(718, 592)
+        Me.Controls.Add(Me.CrystalReportViewer1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Frm_Report_Contract"
+        Me.Text = "Frm_Report_Contract"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Txt_ContId As TextBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents dtpick_exdate As DateTimePicker
+    Friend WithEvents Rdb_ByContId As RadioButton
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
+End Class
