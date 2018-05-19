008cac9fd5bc2bb44857b054407d43be8f8498fe
Hadi Deeb <kadu.coolboy@live.com>
2018-05-19 03:15:35 +0300

    connects done after pain
    fuck (') this shit killed my time

--- SafeBoxes_TS2_ALAFAK/Frm_Connects.Designer.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_Connects.Designer.vb
@@ -23,22 +23,28 @@ Partial Class Frm_Connects
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.DGV_Connects = New System.Windows.Forms.DataGridView()
-        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Btn_Submit = New System.Windows.Forms.Button()
         Me.Btn_Connects = New System.Windows.Forms.Button()
-        Me.MaterialLabel3 = New MaterialSkin.Controls.MaterialLabel()
-        Me.Txt_ClientName = New System.Windows.Forms.TextBox()
-        Me.Txt_ClientId = New System.Windows.Forms.TextBox()
-        Me.MaterialLabel7 = New MaterialSkin.Controls.MaterialLabel()
-        Me.DTPick_Expire = New System.Windows.Forms.DateTimePicker()
-        Me.MaterialLabel2 = New MaterialSkin.Controls.MaterialLabel()
         Me.Lbl_infovId = New MaterialSkin.Controls.MaterialLabel()
         Me.Lbl_EmpId = New MaterialSkin.Controls.MaterialLabel()
         Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
         Me.txt_note = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.MaterialLabel3 = New MaterialSkin.Controls.MaterialLabel()
+        Me.Txt_EmpName = New System.Windows.Forms.TextBox()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.DGV_Connects, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'DGV_Connects
@@ -53,11 +59,11 @@ Partial Class Frm_Connects
         Me.DGV_Connects.BackgroundColor = System.Drawing.Color.White
         Me.DGV_Connects.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
         Me.DGV_Connects.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DGV_Connects.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3})
+        Me.DGV_Connects.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column5, Me.Column3, Me.Column4})
         Me.DGV_Connects.Cursor = System.Windows.Forms.Cursors.Hand
         Me.DGV_Connects.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
         Me.DGV_Connects.GridColor = System.Drawing.Color.PowderBlue
-        Me.DGV_Connects.Location = New System.Drawing.Point(15, 135)
+        Me.DGV_Connects.Location = New System.Drawing.Point(15, 140)
         Me.DGV_Connects.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.DGV_Connects.MultiSelect = False
         Me.DGV_Connects.Name = "DGV_Connects"
@@ -66,29 +72,8 @@ Partial Class Frm_Connects
         Me.DGV_Connects.RowHeadersVisible = False
         Me.DGV_Connects.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders
         Me.DGV_Connects.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.DGV_Connects.Size = New System.Drawing.Size(653, 351)
+        Me.DGV_Connects.Size = New System.Drawing.Size(653, 346)
         Me.DGV_Connects.TabIndex = 86
-        '
-        'Column1
-        '
-        Me.Column1.FillWeight = 40.0!
-        Me.Column1.HeaderText = "Client ID"
-        Me.Column1.Name = "Column1"
-        Me.Column1.ReadOnly = True
-        '
-        'Column2
-        '
-        Me.Column2.FillWeight = 90.0!
-        Me.Column2.HeaderText = "Client Name"
-        Me.Column2.Name = "Column2"
-        Me.Column2.ReadOnly = True
-        '
-        'Column3
-        '
-        Me.Column3.FillWeight = 73.85786!
-        Me.Column3.HeaderText = "Date of Birth"
-        Me.Column3.Name = "Column3"
-        Me.Column3.ReadOnly = True
         '
         'Btn_Submit
         '
@@ -103,86 +88,12 @@ Partial Class Frm_Connects
         '
         'Btn_Connects
         '
-        Me.Btn_Connects.Enabled = False
-        Me.Btn_Connects.Location = New System.Drawing.Point(592, 65)
+        Me.Btn_Connects.Location = New System.Drawing.Point(593, 104)
         Me.Btn_Connects.Name = "Btn_Connects"
         Me.Btn_Connects.Size = New System.Drawing.Size(75, 26)
         Me.Btn_Connects.TabIndex = 84
         Me.Btn_Connects.Text = "Connect"
         Me.Btn_Connects.UseVisualStyleBackColor = True
-        '
-        'MaterialLabel3
-        '
-        Me.MaterialLabel3.AutoSize = True
-        Me.MaterialLabel3.Depth = 0
-        Me.MaterialLabel3.Font = New System.Drawing.Font("Roboto", 11.0!)
-        Me.MaterialLabel3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel3.Location = New System.Drawing.Point(312, 41)
-        Me.MaterialLabel3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.MaterialLabel3.MouseState = MaterialSkin.MouseState.HOVER
-        Me.MaterialLabel3.Name = "MaterialLabel3"
-        Me.MaterialLabel3.Size = New System.Drawing.Size(96, 19)
-        Me.MaterialLabel3.TabIndex = 83
-        Me.MaterialLabel3.Text = "Client Name:"
-        '
-        'Txt_ClientName
-        '
-        Me.Txt_ClientName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Txt_ClientName.Location = New System.Drawing.Point(315, 65)
-        Me.Txt_ClientName.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Txt_ClientName.Name = "Txt_ClientName"
-        Me.Txt_ClientName.ReadOnly = True
-        Me.Txt_ClientName.Size = New System.Drawing.Size(270, 26)
-        Me.Txt_ClientName.TabIndex = 82
-        '
-        'Txt_ClientId
-        '
-        Me.Txt_ClientId.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Txt_ClientId.Location = New System.Drawing.Point(159, 65)
-        Me.Txt_ClientId.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Txt_ClientId.Name = "Txt_ClientId"
-        Me.Txt_ClientId.Size = New System.Drawing.Size(148, 26)
-        Me.Txt_ClientId.TabIndex = 80
-        '
-        'MaterialLabel7
-        '
-        Me.MaterialLabel7.AutoSize = True
-        Me.MaterialLabel7.Depth = 0
-        Me.MaterialLabel7.Font = New System.Drawing.Font("Roboto", 11.0!)
-        Me.MaterialLabel7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel7.Location = New System.Drawing.Point(156, 41)
-        Me.MaterialLabel7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.MaterialLabel7.MouseState = MaterialSkin.MouseState.HOVER
-        Me.MaterialLabel7.Name = "MaterialLabel7"
-        Me.MaterialLabel7.Size = New System.Drawing.Size(70, 19)
-        Me.MaterialLabel7.TabIndex = 81
-        Me.MaterialLabel7.Text = "Client ID:"
-        '
-        'DTPick_Expire
-        '
-        Me.DTPick_Expire.CustomFormat = "dd/mm/yyyy"
-        Me.DTPick_Expire.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.DTPick_Expire.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DTPick_Expire.Location = New System.Drawing.Point(14, 65)
-        Me.DTPick_Expire.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
-        Me.DTPick_Expire.MinDate = New Date(2018, 5, 4, 0, 0, 0, 0)
-        Me.DTPick_Expire.Name = "DTPick_Expire"
-        Me.DTPick_Expire.Size = New System.Drawing.Size(137, 26)
-        Me.DTPick_Expire.TabIndex = 78
-        '
-        'MaterialLabel2
-        '
-        Me.MaterialLabel2.AutoSize = True
-        Me.MaterialLabel2.Depth = 0
-        Me.MaterialLabel2.Font = New System.Drawing.Font("Roboto", 11.0!)
-        Me.MaterialLabel2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel2.Location = New System.Drawing.Point(11, 41)
-        Me.MaterialLabel2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.MaterialLabel2.MouseState = MaterialSkin.MouseState.HOVER
-        Me.MaterialLabel2.Name = "MaterialLabel2"
-        Me.MaterialLabel2.Size = New System.Drawing.Size(104, 19)
-        Me.MaterialLabel2.TabIndex = 79
-        Me.MaterialLabel2.Text = "Connect Date:"
         '
         'Lbl_infovId
         '
@@ -218,62 +129,192 @@ Partial Class Frm_Connects
         Me.MaterialLabel1.Depth = 0
         Me.MaterialLabel1.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel1.Location = New System.Drawing.Point(16, 103)
+        Me.MaterialLabel1.Location = New System.Drawing.Point(14, 108)
         Me.MaterialLabel1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.MaterialLabel1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel1.Name = "MaterialLabel1"
-        Me.MaterialLabel1.Size = New System.Drawing.Size(114, 19)
+        Me.MaterialLabel1.Size = New System.Drawing.Size(100, 19)
         Me.MaterialLabel1.TabIndex = 88
-        Me.MaterialLabel1.Text = "Connect Notes:"
+        Me.MaterialLabel1.Text = "Answer Note:"
         '
         'txt_note
         '
+        Me.txt_note.Enabled = False
         Me.txt_note.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.txt_note.Location = New System.Drawing.Point(138, 99)
+        Me.txt_note.Location = New System.Drawing.Point(120, 104)
         Me.txt_note.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txt_note.Name = "txt_note"
-        Me.txt_note.Size = New System.Drawing.Size(530, 26)
+        Me.txt_note.Size = New System.Drawing.Size(466, 26)
         Me.txt_note.TabIndex = 87
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.GroupBox1.Location = New System.Drawing.Point(19, 31)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(219, 65)
+        Me.GroupBox1.TabIndex = 89
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Answer :"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(101, 35)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(47, 24)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "No"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(101, 12)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(55, 24)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Yes"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Enabled = False
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.GroupBox2.Location = New System.Drawing.Point(244, 33)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(219, 63)
+        Me.GroupBox2.TabIndex = 90
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Result :"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(93, 33)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(47, 24)
+        Me.RadioButton3.TabIndex = 3
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "No"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(93, 10)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(55, 24)
+        Me.RadioButton4.TabIndex = 2
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Yes"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'MaterialLabel3
+        '
+        Me.MaterialLabel3.AutoSize = True
+        Me.MaterialLabel3.Depth = 0
+        Me.MaterialLabel3.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.MaterialLabel3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialLabel3.Location = New System.Drawing.Point(470, 31)
+        Me.MaterialLabel3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.MaterialLabel3.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialLabel3.Name = "MaterialLabel3"
+        Me.MaterialLabel3.Size = New System.Drawing.Size(123, 19)
+        Me.MaterialLabel3.TabIndex = 92
+        Me.MaterialLabel3.Text = "Employee Name:"
+        '
+        'Txt_EmpName
+        '
+        Me.Txt_EmpName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Txt_EmpName.Location = New System.Drawing.Point(474, 57)
+        Me.Txt_EmpName.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Txt_EmpName.Name = "Txt_EmpName"
+        Me.Txt_EmpName.ReadOnly = True
+        Me.Txt_EmpName.Size = New System.Drawing.Size(187, 26)
+        Me.Txt_EmpName.TabIndex = 91
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Info Voucher Id"
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Answer"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Note"
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Result"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Employee"
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
         '
         'Frm_Connects
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(679, 530)
+        Me.Controls.Add(Me.MaterialLabel3)
+        Me.Controls.Add(Me.Txt_EmpName)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.MaterialLabel1)
         Me.Controls.Add(Me.txt_note)
         Me.Controls.Add(Me.DGV_Connects)
         Me.Controls.Add(Me.Btn_Submit)
         Me.Controls.Add(Me.Btn_Connects)
-        Me.Controls.Add(Me.MaterialLabel3)
-        Me.Controls.Add(Me.Txt_ClientName)
-        Me.Controls.Add(Me.Txt_ClientId)
-        Me.Controls.Add(Me.MaterialLabel7)
-        Me.Controls.Add(Me.DTPick_Expire)
-        Me.Controls.Add(Me.MaterialLabel2)
         Me.Controls.Add(Me.Lbl_infovId)
         Me.Controls.Add(Me.Lbl_EmpId)
         Me.Name = "Frm_Connects"
         Me.Text = "Frm_Connects"
         CType(Me.DGV_Connects, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents DGV_Connects As DataGridView
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents Btn_Submit As Button
     Friend WithEvents Btn_Connects As Button
-    Friend WithEvents MaterialLabel3 As MaterialSkin.Controls.MaterialLabel
-    Friend WithEvents Txt_ClientName As TextBox
-    Friend WithEvents Txt_ClientId As TextBox
-    Friend WithEvents MaterialLabel7 As MaterialSkin.Controls.MaterialLabel
-    Friend WithEvents DTPick_Expire As DateTimePicker
-    Friend WithEvents MaterialLabel2 As MaterialSkin.Controls.MaterialLabel
     Friend WithEvents Lbl_infovId As MaterialSkin.Controls.MaterialLabel
     Friend WithEvents Lbl_EmpId As MaterialSkin.Controls.MaterialLabel
     Friend WithEvents MaterialLabel1 As MaterialSkin.Controls.MaterialLabel
     Friend WithEvents txt_note As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents MaterialLabel3 As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents Txt_EmpName As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
 End Class
--- SafeBoxes_TS2_ALAFAK/Frm_Connects.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_Connects.vb
@@ -2,21 +2,21 @@
     Dim infoVouchid As Integer
     Dim EmpId As Integer
     Dim ds As New DataSet
+    Dim Ans As String = "no"
+    Dim Res As String = "no"
     Private Sub Frm_Connects_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         infoVouchid = Frm_main.infovouchid
         EmpId = Frm_main.loggedEmpId
         Lbl_infovId.Text = "InfoVoucher ID: " & infoVouchid
         Lbl_EmpId.Text = "Employee ID: " & EmpId
-    End Sub
-
-    Private Sub Txt_ClientId_TextChanged(sender As Object, e As EventArgs) Handles Txt_ClientId.TextChanged
         Try
-            ds = ReadQueryOut("SELECT ClientId, ClientFName + ' ' + ClientLName, ClientDOB FROM Clients WHERE ClientId = " & Txt_ClientId.Text)
-            Txt_ClientName.Text = ds.Tables(0).Rows(0).Item(1)
+            ds = ReadQueryOut("SELECT EmpId, EmpFName + ' ' + EmpLName FROM Employees WHERE EmpId = " & EmpId)
+            Txt_EmpName.Text = ds.Tables(0).Rows(0).Item(1)
         Catch ex As Exception
-            Txt_ClientName.Text = ""
+            Txt_EmpName.Text = ""
         End Try
     End Sub
+
     Private Sub DGV_Visits_SelectionChanged(sender As Object, e As EventArgs)
         If DGV_Connects.Rows.Count > 0 Then
             Btn_Submit.Enabled = True
@@ -24,14 +24,59 @@
             Btn_Submit.Enabled = False
         End If
     End Sub
-    Private Sub Txt_ClientId_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Txt_ClientId.KeyPress
-        Only_Number(Txt_ClientId, e)
-    End Sub
+
     Private Sub DGV_Visits_SelectionChanged_1(sender As Object, e As EventArgs) Handles DGV_Connects.SelectionChanged
         If DGV_Connects.Rows.Count > 0 Then
             Btn_Submit.Enabled = True
         Else
             Btn_Submit.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Btn_Connects_Click(sender As Object, e As EventArgs) Handles Btn_Connects.Click
+        If txt_note.Text <> "" Then
+
+            DGV_Connects.Rows.Add(infoVouchid, Ans, txt_note.Text, Res, Txt_EmpName.Text)
+
+        Else
+            MessageBox.Show("Insert a note first !")
+        End If
+        Ans = "no"
+        Res = "no"
+        RadioButton2.Checked = True
+        RadioButton3.Checked = True
+        txt_note.Text = "No Answer"
+    End Sub
+
+    Private Sub Btn_Submit_Click(sender As Object, e As EventArgs) Handles Btn_Submit.Click
+        For i As Integer = 0 To DGV_Connects.Rows.Count - 1
+            Dim theNewId As Integer = GenID("Connects", "ConnectId")
+            ExecuteQuery("INSERT INTO Connects VALUES(" & theNewId & ", date(), date(), " & DGV_Connects.Rows(i).Cells(1).Value & ", " & DGV_Connects.Rows(i).Cells(3).Value & ", " & infoVouchid & ", " & EmpId & ")")
+            ExecuteQuery("INSERT INTO AnswerNote VALUES(" & theNewId & ", date(), date(), " & DGV_Connects.Rows(i).Cells(1).Value & ", " & DGV_Connects.Rows(i).Cells(3).Value & ", '" & DGV_Connects.Rows(i).Cells(2).Value & "', " & infoVouchid & ", " & EmpId & ")")
+        Next
+        Me.Dispose()
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged_1(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged, RadioButton1.CheckedChanged
+        If RadioButton1.Checked Then
+            Ans = "yes"
+            txt_note.Text = ""
+            GroupBox2.Enabled = True
+            txt_note.Enabled = True
+        Else
+            Ans = "no"
+            txt_note.Text = "No Answer"
+            txt_note.Enabled = False
+            GroupBox2.Enabled = False
+            RadioButton3.Checked = True
+        End If
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged_1(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged, RadioButton3.CheckedChanged
+        If RadioButton4.Checked Then
+            Res = "yes"
+        Else
+            Res = "no"
         End If
     End Sub
 End Class
