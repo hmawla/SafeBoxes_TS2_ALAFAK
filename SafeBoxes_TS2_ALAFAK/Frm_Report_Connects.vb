a70fa2021625d97f44d8bd97c8c723cac2615828
Hadi Deeb <kadu.coolboy@live.com>
2018-05-20 15:41:30 +0300

    added connects report

---
+++ SafeBoxes_TS2_ALAFAK/Frm_Report_Connects.vb
@@ -0,0 +1,8 @@
+﻿Public Class Frm_Report_Connects
+    Dim RptDbDataSet As New DataSet
+    Dim CrysReport As New Rpt_Connects
+    Private Sub Frm_Report_Connects_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RptV_connects.ReportSource = CrysReport
+        RptV_connects.RefreshReport()
+    End Sub
+End Class
