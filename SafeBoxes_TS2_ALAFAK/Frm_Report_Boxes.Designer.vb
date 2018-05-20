89fb7f664fa5a07ebabc7ea3aa3b2cabd101f5d5
Hadi Deeb <kadu.coolboy@live.com>
2018-05-20 15:52:24 +0300

    boxes report

---
+++ SafeBoxes_TS2_ALAFAK/Frm_Report_Boxes.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Report_Boxes
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RptV_Boxes = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.dtpick_exdate = New System.Windows.Forms.DateTimePicker()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RptV_Boxes
+        '
+        Me.RptV_Boxes.ActiveViewIndex = -1
+        Me.RptV_Boxes.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RptV_Boxes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RptV_Boxes.Cursor = System.Windows.Forms.Cursors.Default
+        Me.RptV_Boxes.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RptV_Boxes.Location = New System.Drawing.Point(12, 152)
+        Me.RptV_Boxes.Name = "RptV_Boxes"
+        Me.RptV_Boxes.Size = New System.Drawing.Size(685, 343)
+        Me.RptV_Boxes.TabIndex = 22
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.dtpick_exdate)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(679, 134)
+        Me.GroupBox1.TabIndex = 21
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Filter"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 57)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(234, 27)
+        Me.TextBox1.TabIndex = 12
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(250, 25)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(165, 24)
+        Me.RadioButton2.TabIndex = 15
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "By Date (FROM-TO)"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'dtpick_exdate
+        '
+        Me.dtpick_exdate.CustomFormat = "dd/mm/yyyy"
+        Me.dtpick_exdate.Font = New System.Drawing.Font("Roboto", 12.0!)
+        Me.dtpick_exdate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpick_exdate.Location = New System.Drawing.Point(466, 55)
+        Me.dtpick_exdate.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.dtpick_exdate.MinDate = New Date(2018, 5, 4, 0, 0, 0, 0)
+        Me.dtpick_exdate.Name = "dtpick_exdate"
+        Me.dtpick_exdate.Size = New System.Drawing.Size(204, 27)
+        Me.dtpick_exdate.TabIndex = 10
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 25)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(94, 24)
+        Me.RadioButton1.TabIndex = 16
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "By Box ID"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "dd/mm/yyyy"
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Roboto", 12.0!)
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(250, 55)
+        Me.DateTimePicker1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.DateTimePicker1.MinDate = New Date(2018, 5, 4, 0, 0, 0, 0)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(204, 27)
+        Me.DateTimePicker1.TabIndex = 11
+        '
+        'Frm_Report_Boxes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(709, 507)
+        Me.Controls.Add(Me.RptV_Boxes)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Frm_Report_Boxes"
+        Me.Text = "Frm_Report_Boxes"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RptV_Boxes As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents dtpick_exdate As DateTimePicker
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+End Class
