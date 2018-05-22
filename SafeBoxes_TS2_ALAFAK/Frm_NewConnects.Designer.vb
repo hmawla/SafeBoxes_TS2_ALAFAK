43165fe3cf0b739d7499e88d4b0e8223103cbbfc
Pedro Marthon <pedro_marthon@live.com>
2018-05-22 15:42:29 +0300

    materialized connects

--- SafeBoxes_TS2_ALAFAK/Frm_NewConnects.Designer.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_NewConnects.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Frm_NewConnects
-    Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
@@ -22,31 +22,25 @@ Partial Class Frm_NewConnects
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Btn_Submit = New System.Windows.Forms.Button()
         Me.Lbl_infovId = New MaterialSkin.Controls.MaterialLabel()
         Me.Lbl_EmpId = New MaterialSkin.Controls.MaterialLabel()
+        Me.MaterialLabel2 = New MaterialSkin.Controls.MaterialLabel()
+        Me.Rdb_AnswerN = New MaterialSkin.Controls.MaterialRadioButton()
+        Me.Rdb_AnswerY = New MaterialSkin.Controls.MaterialRadioButton()
+        Me.Txt_AnswerNote = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.Btn_Submit = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.Chk_AnswerNote = New MaterialSkin.Controls.MaterialCheckBox()
         Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
-        Me.txt_note = New System.Windows.Forms.TextBox()
+        Me.Rdb_ResultY = New MaterialSkin.Controls.MaterialRadioButton()
+        Me.Rdb_ResultN = New MaterialSkin.Controls.MaterialRadioButton()
+        Me.SelectEmployee = New MaterialSkin.Controls.MaterialContextMenuStrip()
+        Me.SelectDifferentEmployeeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.SelectEmployee.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'Btn_Submit
-        '
-        Me.Btn_Submit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Btn_Submit.Location = New System.Drawing.Point(535, 160)
-        Me.Btn_Submit.Margin = New System.Windows.Forms.Padding(4)
-        Me.Btn_Submit.Name = "Btn_Submit"
-        Me.Btn_Submit.Size = New System.Drawing.Size(112, 40)
-        Me.Btn_Submit.TabIndex = 85
-        Me.Btn_Submit.Text = "Submit"
-        Me.Btn_Submit.UseVisualStyleBackColor = True
         '
         'Lbl_infovId
         '
@@ -54,7 +48,7 @@ Partial Class Frm_NewConnects
         Me.Lbl_infovId.Depth = 0
         Me.Lbl_infovId.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.Lbl_infovId.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Lbl_infovId.Location = New System.Drawing.Point(16, 14)
+        Me.Lbl_infovId.Location = New System.Drawing.Point(23, 77)
         Me.Lbl_infovId.Margin = New System.Windows.Forms.Padding(14, 0, 14, 0)
         Me.Lbl_infovId.MouseState = MaterialSkin.MouseState.HOVER
         Me.Lbl_infovId.Name = "Lbl_infovId"
@@ -65,10 +59,11 @@ Partial Class Frm_NewConnects
         'Lbl_EmpId
         '
         Me.Lbl_EmpId.AutoSize = True
+        Me.Lbl_EmpId.ContextMenuStrip = Me.SelectEmployee
         Me.Lbl_EmpId.Depth = 0
         Me.Lbl_EmpId.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.Lbl_EmpId.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Lbl_EmpId.Location = New System.Drawing.Point(214, 14)
+        Me.Lbl_EmpId.Location = New System.Drawing.Point(222, 77)
         Me.Lbl_EmpId.Margin = New System.Windows.Forms.Padding(14, 0, 14, 0)
         Me.Lbl_EmpId.MouseState = MaterialSkin.MouseState.HOVER
         Me.Lbl_EmpId.Name = "Lbl_EmpId"
@@ -76,123 +71,206 @@ Partial Class Frm_NewConnects
         Me.Lbl_EmpId.TabIndex = 76
         Me.Lbl_EmpId.Text = "Employee ID: <EmpId>"
         '
+        'MaterialLabel2
+        '
+        Me.MaterialLabel2.AutoSize = True
+        Me.MaterialLabel2.Depth = 0
+        Me.MaterialLabel2.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.MaterialLabel2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialLabel2.Location = New System.Drawing.Point(12, 23)
+        Me.MaterialLabel2.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialLabel2.Name = "MaterialLabel2"
+        Me.MaterialLabel2.Size = New System.Drawing.Size(125, 19)
+        Me.MaterialLabel2.TabIndex = 93
+        Me.MaterialLabel2.Text = "Client Answered?"
+        '
+        'Rdb_AnswerN
+        '
+        Me.Rdb_AnswerN.AutoSize = True
+        Me.Rdb_AnswerN.Checked = True
+        Me.Rdb_AnswerN.Depth = 0
+        Me.Rdb_AnswerN.Font = New System.Drawing.Font("Roboto", 10.0!)
+        Me.Rdb_AnswerN.Location = New System.Drawing.Point(16, 72)
+        Me.Rdb_AnswerN.Margin = New System.Windows.Forms.Padding(0)
+        Me.Rdb_AnswerN.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.Rdb_AnswerN.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Rdb_AnswerN.Name = "Rdb_AnswerN"
+        Me.Rdb_AnswerN.Ripple = True
+        Me.Rdb_AnswerN.Size = New System.Drawing.Size(47, 30)
+        Me.Rdb_AnswerN.TabIndex = 94
+        Me.Rdb_AnswerN.TabStop = True
+        Me.Rdb_AnswerN.Text = "No"
+        Me.Rdb_AnswerN.UseVisualStyleBackColor = True
+        '
+        'Rdb_AnswerY
+        '
+        Me.Rdb_AnswerY.AutoSize = True
+        Me.Rdb_AnswerY.Depth = 0
+        Me.Rdb_AnswerY.Font = New System.Drawing.Font("Roboto", 10.0!)
+        Me.Rdb_AnswerY.Location = New System.Drawing.Point(16, 42)
+        Me.Rdb_AnswerY.Margin = New System.Windows.Forms.Padding(0)
+        Me.Rdb_AnswerY.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.Rdb_AnswerY.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Rdb_AnswerY.Name = "Rdb_AnswerY"
+        Me.Rdb_AnswerY.Ripple = True
+        Me.Rdb_AnswerY.Size = New System.Drawing.Size(52, 30)
+        Me.Rdb_AnswerY.TabIndex = 95
+        Me.Rdb_AnswerY.Text = "Yes"
+        Me.Rdb_AnswerY.UseVisualStyleBackColor = True
+        '
+        'Txt_AnswerNote
+        '
+        Me.Txt_AnswerNote.Depth = 0
+        Me.Txt_AnswerNote.Enabled = False
+        Me.Txt_AnswerNote.Hint = ""
+        Me.Txt_AnswerNote.Location = New System.Drawing.Point(27, 244)
+        Me.Txt_AnswerNote.MaxLength = 32767
+        Me.Txt_AnswerNote.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Txt_AnswerNote.Name = "Txt_AnswerNote"
+        Me.Txt_AnswerNote.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.Txt_AnswerNote.SelectedText = ""
+        Me.Txt_AnswerNote.SelectionLength = 0
+        Me.Txt_AnswerNote.SelectionStart = 0
+        Me.Txt_AnswerNote.Size = New System.Drawing.Size(338, 23)
+        Me.Txt_AnswerNote.TabIndex = 96
+        Me.Txt_AnswerNote.TabStop = False
+        Me.Txt_AnswerNote.UseSystemPasswordChar = False
+        '
+        'Btn_Submit
+        '
+        Me.Btn_Submit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Btn_Submit.AutoSize = True
+        Me.Btn_Submit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Btn_Submit.Depth = 0
+        Me.Btn_Submit.Icon = Nothing
+        Me.Btn_Submit.Location = New System.Drawing.Point(307, 300)
+        Me.Btn_Submit.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Btn_Submit.Name = "Btn_Submit"
+        Me.Btn_Submit.Primary = True
+        Me.Btn_Submit.Size = New System.Drawing.Size(71, 36)
+        Me.Btn_Submit.TabIndex = 98
+        Me.Btn_Submit.Text = "Submit"
+        Me.Btn_Submit.UseVisualStyleBackColor = True
+        '
+        'Chk_AnswerNote
+        '
+        Me.Chk_AnswerNote.AutoSize = True
+        Me.Chk_AnswerNote.Depth = 0
+        Me.Chk_AnswerNote.Enabled = False
+        Me.Chk_AnswerNote.Font = New System.Drawing.Font("Roboto", 10.0!)
+        Me.Chk_AnswerNote.Location = New System.Drawing.Point(27, 211)
+        Me.Chk_AnswerNote.Margin = New System.Windows.Forms.Padding(0)
+        Me.Chk_AnswerNote.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.Chk_AnswerNote.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Chk_AnswerNote.Name = "Chk_AnswerNote"
+        Me.Chk_AnswerNote.Ripple = True
+        Me.Chk_AnswerNote.Size = New System.Drawing.Size(113, 30)
+        Me.Chk_AnswerNote.TabIndex = 99
+        Me.Chk_AnswerNote.Text = "Answer Note:"
+        Me.Chk_AnswerNote.UseVisualStyleBackColor = True
+        '
         'MaterialLabel1
         '
         Me.MaterialLabel1.AutoSize = True
         Me.MaterialLabel1.Depth = 0
         Me.MaterialLabel1.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel1.Location = New System.Drawing.Point(109, 31)
-        Me.MaterialLabel1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.MaterialLabel1.Location = New System.Drawing.Point(6, 23)
         Me.MaterialLabel1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel1.Name = "MaterialLabel1"
-        Me.MaterialLabel1.Size = New System.Drawing.Size(100, 19)
-        Me.MaterialLabel1.TabIndex = 88
-        Me.MaterialLabel1.Text = "Answer Note:"
+        Me.MaterialLabel1.Size = New System.Drawing.Size(132, 19)
+        Me.MaterialLabel1.TabIndex = 100
+        Me.MaterialLabel1.Text = "Called with result?"
         '
-        'txt_note
+        'Rdb_ResultY
         '
-        Me.txt_note.Enabled = False
-        Me.txt_note.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txt_note.Location = New System.Drawing.Point(113, 56)
-        Me.txt_note.Margin = New System.Windows.Forms.Padding(6, 7, 6, 7)
-        Me.txt_note.Name = "txt_note"
-        Me.txt_note.Size = New System.Drawing.Size(390, 27)
-        Me.txt_note.TabIndex = 87
+        Me.Rdb_ResultY.AutoSize = True
+        Me.Rdb_ResultY.Depth = 0
+        Me.Rdb_ResultY.Font = New System.Drawing.Font("Roboto", 10.0!)
+        Me.Rdb_ResultY.Location = New System.Drawing.Point(10, 42)
+        Me.Rdb_ResultY.Margin = New System.Windows.Forms.Padding(3, 0, 3, 0)
+        Me.Rdb_ResultY.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.Rdb_ResultY.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Rdb_ResultY.Name = "Rdb_ResultY"
+        Me.Rdb_ResultY.Ripple = True
+        Me.Rdb_ResultY.Size = New System.Drawing.Size(52, 30)
+        Me.Rdb_ResultY.TabIndex = 102
+        Me.Rdb_ResultY.Text = "Yes"
+        Me.Rdb_ResultY.UseVisualStyleBackColor = True
+        '
+        'Rdb_ResultN
+        '
+        Me.Rdb_ResultN.AutoSize = True
+        Me.Rdb_ResultN.Checked = True
+        Me.Rdb_ResultN.Depth = 0
+        Me.Rdb_ResultN.Font = New System.Drawing.Font("Roboto", 10.0!)
+        Me.Rdb_ResultN.Location = New System.Drawing.Point(10, 72)
+        Me.Rdb_ResultN.Margin = New System.Windows.Forms.Padding(0)
+        Me.Rdb_ResultN.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.Rdb_ResultN.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Rdb_ResultN.Name = "Rdb_ResultN"
+        Me.Rdb_ResultN.Ripple = True
+        Me.Rdb_ResultN.Size = New System.Drawing.Size(47, 30)
+        Me.Rdb_ResultN.TabIndex = 101
+        Me.Rdb_ResultN.TabStop = True
+        Me.Rdb_ResultN.Text = "No"
+        Me.Rdb_ResultN.UseVisualStyleBackColor = True
+        '
+        'SelectEmployee
+        '
+        Me.SelectEmployee.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.SelectEmployee.Depth = 0
+        Me.SelectEmployee.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectDifferentEmployeeToolStripMenuItem})
+        Me.SelectEmployee.MouseState = MaterialSkin.MouseState.HOVER
+        Me.SelectEmployee.Name = "SelectEmployee"
+        Me.SelectEmployee.Size = New System.Drawing.Size(210, 26)
+        '
+        'SelectDifferentEmployeeToolStripMenuItem
+        '
+        Me.SelectDifferentEmployeeToolStripMenuItem.Name = "SelectDifferentEmployeeToolStripMenuItem"
+        Me.SelectDifferentEmployeeToolStripMenuItem.Size = New System.Drawing.Size(209, 22)
+        Me.SelectDifferentEmployeeToolStripMenuItem.Text = "Select Different Employee"
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.RadioButton2)
-        Me.GroupBox1.Controls.Add(Me.RadioButton1)
         Me.GroupBox1.Controls.Add(Me.MaterialLabel1)
-        Me.GroupBox1.Controls.Add(Me.txt_note)
-        Me.GroupBox1.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox1.Location = New System.Drawing.Point(19, 36)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Controls.Add(Me.Rdb_ResultN)
+        Me.GroupBox1.Controls.Add(Me.Rdb_ResultY)
+        Me.GroupBox1.Location = New System.Drawing.Point(27, 99)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
-        Me.GroupBox1.Size = New System.Drawing.Size(621, 100)
-        Me.GroupBox1.TabIndex = 89
+        Me.GroupBox1.Size = New System.Drawing.Size(167, 109)
+        Me.GroupBox1.TabIndex = 104
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Answer :"
-        '
-        'RadioButton2
-        '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(8, 59)
-        Me.RadioButton2.Margin = New System.Windows.Forms.Padding(4)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(47, 24)
-        Me.RadioButton2.TabIndex = 1
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "No"
-        Me.RadioButton2.UseVisualStyleBackColor = True
-        '
-        'RadioButton1
-        '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(8, 28)
-        Me.RadioButton1.Margin = New System.Windows.Forms.Padding(4)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(53, 24)
-        Me.RadioButton1.TabIndex = 0
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "Yes"
-        Me.RadioButton1.UseVisualStyleBackColor = True
         '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.RadioButton3)
-        Me.GroupBox2.Controls.Add(Me.RadioButton4)
-        Me.GroupBox2.Enabled = False
-        Me.GroupBox2.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox2.Location = New System.Drawing.Point(27, 144)
-        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox2.Controls.Add(Me.MaterialLabel2)
+        Me.GroupBox2.Controls.Add(Me.Rdb_AnswerN)
+        Me.GroupBox2.Controls.Add(Me.Rdb_AnswerY)
+        Me.GroupBox2.Location = New System.Drawing.Point(200, 99)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4)
-        Me.GroupBox2.Size = New System.Drawing.Size(497, 61)
-        Me.GroupBox2.TabIndex = 90
+        Me.GroupBox2.Size = New System.Drawing.Size(165, 109)
+        Me.GroupBox2.TabIndex = 105
         Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "Result :"
-        '
-        'RadioButton3
-        '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(69, 28)
-        Me.RadioButton3.Margin = New System.Windows.Forms.Padding(4)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(47, 24)
-        Me.RadioButton3.TabIndex = 3
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "No"
-        Me.RadioButton3.UseVisualStyleBackColor = True
-        '
-        'RadioButton4
-        '
-        Me.RadioButton4.AutoSize = True
-        Me.RadioButton4.Location = New System.Drawing.Point(8, 25)
-        Me.RadioButton4.Margin = New System.Windows.Forms.Padding(4)
-        Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(53, 24)
-        Me.RadioButton4.TabIndex = 2
-        Me.RadioButton4.TabStop = True
-        Me.RadioButton4.Text = "Yes"
-        Me.RadioButton4.UseVisualStyleBackColor = True
         '
         'Frm_NewConnects
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(660, 213)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(390, 348)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Chk_AnswerNote)
         Me.Controls.Add(Me.Btn_Submit)
+        Me.Controls.Add(Me.Txt_AnswerNote)
         Me.Controls.Add(Me.Lbl_infovId)
         Me.Controls.Add(Me.Lbl_EmpId)
         Me.Font = New System.Drawing.Font("Roboto", 12.0!)
         Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Frm_NewConnects"
         Me.Text = "Frm_Connects"
+        Me.SelectEmployee.ResumeLayout(False)
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
@@ -201,15 +279,19 @@ Partial Class Frm_NewConnects
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Btn_Submit As Button
     Friend WithEvents Lbl_infovId As MaterialSkin.Controls.MaterialLabel
     Friend WithEvents Lbl_EmpId As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents MaterialLabel2 As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents Rdb_AnswerN As MaterialSkin.Controls.MaterialRadioButton
+    Friend WithEvents Rdb_AnswerY As MaterialSkin.Controls.MaterialRadioButton
+    Friend WithEvents Txt_AnswerNote As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents Btn_Submit As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents Chk_AnswerNote As MaterialSkin.Controls.MaterialCheckBox
     Friend WithEvents MaterialLabel1 As MaterialSkin.Controls.MaterialLabel
-    Friend WithEvents txt_note As TextBox
+    Friend WithEvents Rdb_ResultY As MaterialSkin.Controls.MaterialRadioButton
+    Friend WithEvents Rdb_ResultN As MaterialSkin.Controls.MaterialRadioButton
+    Friend WithEvents SelectEmployee As MaterialSkin.Controls.MaterialContextMenuStrip
+    Friend WithEvents SelectDifferentEmployeeToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents RadioButton2 As RadioButton
-    Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents GroupBox2 As GroupBox
-    Friend WithEvents RadioButton3 As RadioButton
-    Friend WithEvents RadioButton4 As RadioButton
 End Class
--- SafeBoxes_TS2_ALAFAK/Frm_NewConnects.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_NewConnects.vb
@@ -2,60 +2,14 @@
     Dim infoVouchid As Integer
     Dim EmpId As Integer
     Dim ds As New DataSet
-    Dim Ans As String = "no"
-    Dim Res As String = "no"
     Private Sub Frm_Connects_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         infoVouchid = Frm_main.infovouchid
         EmpId = Frm_main.loggedEmpId
         Lbl_infovId.Text = "InfoVoucher ID: " & infoVouchid
         Lbl_EmpId.Text = "Employee ID: " & EmpId
-        'Try
-        '    ds = ReadQueryOut("SELECT EmpId, EmpFName + ' ' + EmpLName FROM Employees WHERE EmpId = " & EmpId)
-        '    Txt_EmpName.Text = ds.Tables(0).Rows(0).Item(1)
-        'Catch ex As Exception
-        '    Txt_EmpName.Text = ""
-        'End Try
-    End Sub
-
-    Private Sub Btn_Connects_Click(sender As Object, e As EventArgs)
-        If txt_note.Text <> "" Then
-            'We dont need to display the employees name in a textbox, make a new variable and put the name in it
-            'DGV_Connects.Rows.Add(infoVouchid, Ans, txt_note.Text, Res, Txt_EmpName.Text)
-
-        Else
-            MessageBox.Show("Insert a note first !")
-        End If
-        Ans = "no"
-        Res = "no"
-        RadioButton2.Checked = True
-        RadioButton3.Checked = True
-        txt_note.Text = "No Answer"
     End Sub
 
     Private Sub Btn_Submit_Click(sender As Object, e As EventArgs) Handles Btn_Submit.Click
-        Dim Note As String = InputBox("Answer Note", "Enter your Answer note below: ", "Note Example")
-    End Sub
-
-    Private Sub RadioButton1_CheckedChanged_1(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged, RadioButton1.CheckedChanged
-        If RadioButton1.Checked Then
-            Ans = "yes"
-            txt_note.Text = ""
-            GroupBox2.Enabled = True
-            txt_note.Enabled = True
-        Else
-            Ans = "no"
-            txt_note.Text = "No Answer"
-            txt_note.Enabled = False
-            GroupBox2.Enabled = False
-            RadioButton3.Checked = True
-        End If
-    End Sub
-
-    Private Sub RadioButton4_CheckedChanged_1(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged, RadioButton3.CheckedChanged
-        If RadioButton4.Checked Then
-            Res = "yes"
-        Else
-            Res = "no"
-        End If
+        'Dim Note As String = InputBox("Answer Note", "Enter your Answer note below: ", "Note Example")
     End Sub
 End Class
