49943de32fc855ed6529dcf162e3a5a1c004cee5
Pedro Marthon <pedro_marthon@live.com>
2018-05-25 12:58:44 +0300

    fixed boxid bug in contract

--- SafeBoxes_TS2_ALAFAK/Frm_Contracts.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_Contracts.vb
@@ -6,6 +6,7 @@
     '==========================================
     Public redeliverDate As Date
     Public contractId As Integer
+    Public boxId As Integer
 
     Private Sub Frm_Contracts_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         FillDGV(dgv_contracts, "SELECT ContId AS ID, ContBDate AS [Contract Date], ContToDate AS [Expire Date], ContPhone1 AS [Phone1], ContPhone2 AS [Phone2], BoxId AS [Box ID], AccountId AS [Account ID], BuildingName AS Address, EmpFName + ' ' + EmpLName AS Employee FROM Contract, Buildings, Employees WHERE Contract.BuildingId = Buildings.BuildingId AND Contract.EmpId = Employees.EmpId AND ContId NOT IN (SELECT ContId FROM ContEnd)")
@@ -19,7 +20,9 @@
 
     Private Sub btn_modcontract_Click(sender As Object, e As EventArgs) Handles btn_modcontract.Click
         contractId = dgv_contracts.SelectedRows(0).Cells(0).Value
+        boxId = dgv_contracts.SelectedRows(0).Cells(5).Value
         Frm_newContract.ShowDialog()
+        boxId = 0
         FillDGV(dgv_contracts, "SELECT ContId AS ID, ContBDate AS [Contract Date], ContToDate AS [Expire Date], ContPhone1 AS [Phone1], ContPhone2 AS [Phone2], BoxId AS [Box ID], AccountId AS [Account ID], BuildingName AS Address, EmpFName + ' ' + EmpLName AS Employee FROM Contract, Buildings, Employees WHERE Contract.BuildingId = Buildings.BuildingId AND Contract.EmpId = Employees.EmpId AND ContId NOT IN (SELECT ContId FROM ContEnd)")
     End Sub
 
