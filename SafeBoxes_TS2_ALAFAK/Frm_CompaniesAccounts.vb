1725a3522e2a58258d733ea82b37a1677b88cff9
Hadi Deeb <kadu.coolboy@live.com>
2018-05-25 16:45:29 +0300

    filters fixing

--- SafeBoxes_TS2_ALAFAK/Frm_CompaniesAccounts.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_CompaniesAccounts.vb
@@ -23,4 +23,34 @@
             End Try
         End If
     End Sub
+
+    Private Sub rdb_byAccid_CheckedChanged(sender As Object, e As EventArgs) Handles rdb_byAccid.CheckedChanged
+        txt_byaccid.Enabled = True
+        txt_cname.Enabled = False
+        txt_cname.Text = ""
+        txt_byaccid.Focus()
+    End Sub
+
+    Private Sub rdb_byCname_CheckedChanged(sender As Object, e As EventArgs) Handles rdb_byCname.CheckedChanged
+        txt_byaccid.Enabled = False
+        txt_cname.Enabled = True
+        txt_byaccid.Text = ""
+        txt_cname.Focus()
+    End Sub
+
+    Private Sub btn_search_Click(sender As Object, e As EventArgs) Handles btn_search.Click
+        btn_reset.Enabled = True
+        If rdb_byAccid.Checked Then
+            FillDGV(dgv_accounts, "SELECT ca.AccountId AS [ACCOUNT ID],CompName AS [COMPANY],ClientFName AS [REPRESENTATOR],SignCardId FROM  CompanyAccounts ca,Company com,Clients c,ClientRepAccount rep,SignCardsAccounts sca WHERE ca.CompId=com.CompId AND rep.ClientId=c.ClientId AND ca.AccountId=sca.AccountId AND ca.AccountId=" & txt_byaccid.Text)
+        Else
+            FillDGV(dgv_accounts, "SELECT ca.AccountId AS [ACCOUNT ID],CompName AS [COMPANY],ClientFName AS [REPRESENTATOR],SignCardId FROM  CompanyAccounts ca,Company com,Clients c,ClientRepAccount rep,SignCardsAccounts sca WHERE ca.CompId=com.CompId AND rep.ClientId=c.ClientId AND ca.AccountId=sca.AccountId AND com.CompName LIKE '%" & txt_cname.Text & "%'")
+        End If
+    End Sub
+
+    Private Sub btn_reset_Click(sender As Object, e As EventArgs) Handles btn_reset.Click
+        btn_reset.Enabled = False
+        txt_byaccid.Text = ""
+        txt_cname.Text = ""
+        FillDGV(dgv_accounts, "SELECT ca.AccountId AS [ACCOUNT ID],CompName AS [COMPANY],ClientFName AS [REPRESENTATOR],SignCardId FROM  CompanyAccounts ca,Company com,Clients c,ClientRepAccount rep,SignCardsAccounts sca WHERE ca.CompId=com.CompId AND rep.ClientId=c.ClientId AND ca.AccountId=sca.AccountId")
+    End Sub
 End Class
