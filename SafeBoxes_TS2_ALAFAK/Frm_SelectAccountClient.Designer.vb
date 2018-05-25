7d827737e956e7c70d2f3fb3305be3a687c23433
Hadi Deeb <kadu.coolboy@live.com>
2018-05-25 18:07:31 +0300

    material selects

--- SafeBoxes_TS2_ALAFAK/Frm_SelectAccountClient.Designer.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_SelectAccountClient.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Frm_SelectAccountClient
-    Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()>
@@ -23,14 +23,14 @@ Partial Class Frm_SelectAccountClient
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.dgv_accounts = New System.Windows.Forms.DataGridView()
-        Me.txt_byid = New System.Windows.Forms.TextBox()
-        Me.btn_search = New System.Windows.Forms.Button()
+        Me.txt_byid = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.btn_search = New MaterialSkin.Controls.MaterialFlatButton()
         Me.grp_search = New System.Windows.Forms.GroupBox()
-        Me.btn_reset = New System.Windows.Forms.Button()
-        Me.rdb_byname = New System.Windows.Forms.RadioButton()
-        Me.txt_byname = New System.Windows.Forms.TextBox()
-        Me.rdb_byid = New System.Windows.Forms.RadioButton()
-        Me.btn_select = New System.Windows.Forms.Button()
+        Me.btn_reset = New MaterialSkin.Controls.MaterialFlatButton()
+        Me.rdb_byname = New MaterialSkin.Controls.MaterialRadioButton()
+        Me.txt_byname = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.rdb_byid = New MaterialSkin.Controls.MaterialRadioButton()
+        Me.btn_select = New MaterialSkin.Controls.MaterialFlatButton()
         CType(Me.dgv_accounts, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.grp_search.SuspendLayout()
         Me.SuspendLayout()
@@ -50,7 +50,7 @@ Partial Class Frm_SelectAccountClient
         Me.dgv_accounts.Cursor = System.Windows.Forms.Cursors.Hand
         Me.dgv_accounts.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
         Me.dgv_accounts.GridColor = System.Drawing.Color.PowderBlue
-        Me.dgv_accounts.Location = New System.Drawing.Point(18, 133)
+        Me.dgv_accounts.Location = New System.Drawing.Point(18, 187)
         Me.dgv_accounts.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.dgv_accounts.MultiSelect = False
         Me.dgv_accounts.Name = "dgv_accounts"
@@ -59,24 +59,40 @@ Partial Class Frm_SelectAccountClient
         Me.dgv_accounts.RowHeadersVisible = False
         Me.dgv_accounts.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders
         Me.dgv_accounts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.dgv_accounts.Size = New System.Drawing.Size(603, 328)
+        Me.dgv_accounts.Size = New System.Drawing.Size(603, 274)
         Me.dgv_accounts.TabIndex = 0
         '
         'txt_byid
         '
+        Me.txt_byid.Depth = 0
+        Me.txt_byid.Hint = ""
         Me.txt_byid.Location = New System.Drawing.Point(6, 58)
         Me.txt_byid.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_byid.MaxLength = 32767
+        Me.txt_byid.MouseState = MaterialSkin.MouseState.HOVER
         Me.txt_byid.Name = "txt_byid"
-        Me.txt_byid.Size = New System.Drawing.Size(148, 26)
+        Me.txt_byid.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txt_byid.SelectedText = ""
+        Me.txt_byid.SelectionLength = 0
+        Me.txt_byid.SelectionStart = 0
+        Me.txt_byid.Size = New System.Drawing.Size(148, 23)
         Me.txt_byid.TabIndex = 1
+        Me.txt_byid.TabStop = False
+        Me.txt_byid.UseSystemPasswordChar = False
         '
         'btn_search
         '
         Me.btn_search.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_search.AutoSize = True
+        Me.btn_search.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn_search.Depth = 0
+        Me.btn_search.Icon = Nothing
         Me.btn_search.Location = New System.Drawing.Point(494, 54)
         Me.btn_search.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_search.MouseState = MaterialSkin.MouseState.HOVER
         Me.btn_search.Name = "btn_search"
-        Me.btn_search.Size = New System.Drawing.Size(70, 35)
+        Me.btn_search.Primary = False
+        Me.btn_search.Size = New System.Drawing.Size(73, 36)
         Me.btn_search.TabIndex = 3
         Me.btn_search.Text = "Search"
         Me.btn_search.UseVisualStyleBackColor = True
@@ -92,7 +108,7 @@ Partial Class Frm_SelectAccountClient
         Me.grp_search.Controls.Add(Me.rdb_byid)
         Me.grp_search.Controls.Add(Me.txt_byid)
         Me.grp_search.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.grp_search.Location = New System.Drawing.Point(18, 12)
+        Me.grp_search.Location = New System.Drawing.Point(18, 66)
         Me.grp_search.Name = "grp_search"
         Me.grp_search.Size = New System.Drawing.Size(603, 113)
         Me.grp_search.TabIndex = 4
@@ -102,11 +118,17 @@ Partial Class Frm_SelectAccountClient
         'btn_reset
         '
         Me.btn_reset.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_reset.AutoSize = True
+        Me.btn_reset.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn_reset.Depth = 0
         Me.btn_reset.Enabled = False
-        Me.btn_reset.Location = New System.Drawing.Point(572, 54)
+        Me.btn_reset.Icon = Nothing
+        Me.btn_reset.Location = New System.Drawing.Point(566, 54)
         Me.btn_reset.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_reset.MouseState = MaterialSkin.MouseState.HOVER
         Me.btn_reset.Name = "btn_reset"
-        Me.btn_reset.Size = New System.Drawing.Size(24, 35)
+        Me.btn_reset.Primary = False
+        Me.btn_reset.Size = New System.Drawing.Size(30, 36)
         Me.btn_reset.TabIndex = 6
         Me.btn_reset.Text = "X"
         Me.btn_reset.UseVisualStyleBackColor = True
@@ -114,9 +136,15 @@ Partial Class Frm_SelectAccountClient
         'rdb_byname
         '
         Me.rdb_byname.AutoSize = True
+        Me.rdb_byname.Depth = 0
+        Me.rdb_byname.Font = New System.Drawing.Font("Roboto", 10.0!)
         Me.rdb_byname.Location = New System.Drawing.Point(162, 26)
+        Me.rdb_byname.Margin = New System.Windows.Forms.Padding(0)
+        Me.rdb_byname.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.rdb_byname.MouseState = MaterialSkin.MouseState.HOVER
         Me.rdb_byname.Name = "rdb_byname"
-        Me.rdb_byname.Size = New System.Drawing.Size(91, 24)
+        Me.rdb_byname.Ripple = True
+        Me.rdb_byname.Size = New System.Drawing.Size(84, 30)
         Me.rdb_byname.TabIndex = 5
         Me.rdb_byname.Text = "By Name"
         Me.rdb_byname.UseVisualStyleBackColor = True
@@ -125,20 +153,36 @@ Partial Class Frm_SelectAccountClient
         '
         Me.txt_byname.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txt_byname.Depth = 0
         Me.txt_byname.Enabled = False
+        Me.txt_byname.Hint = ""
         Me.txt_byname.Location = New System.Drawing.Point(162, 58)
         Me.txt_byname.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_byname.MaxLength = 32767
+        Me.txt_byname.MouseState = MaterialSkin.MouseState.HOVER
         Me.txt_byname.Name = "txt_byname"
-        Me.txt_byname.Size = New System.Drawing.Size(324, 26)
+        Me.txt_byname.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txt_byname.SelectedText = ""
+        Me.txt_byname.SelectionLength = 0
+        Me.txt_byname.SelectionStart = 0
+        Me.txt_byname.Size = New System.Drawing.Size(324, 23)
         Me.txt_byname.TabIndex = 4
+        Me.txt_byname.TabStop = False
+        Me.txt_byname.UseSystemPasswordChar = False
         '
         'rdb_byid
         '
         Me.rdb_byid.AutoSize = True
         Me.rdb_byid.Checked = True
+        Me.rdb_byid.Depth = 0
+        Me.rdb_byid.Font = New System.Drawing.Font("Roboto", 10.0!)
         Me.rdb_byid.Location = New System.Drawing.Point(6, 26)
+        Me.rdb_byid.Margin = New System.Windows.Forms.Padding(0)
+        Me.rdb_byid.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.rdb_byid.MouseState = MaterialSkin.MouseState.HOVER
         Me.rdb_byid.Name = "rdb_byid"
-        Me.rdb_byid.Size = New System.Drawing.Size(66, 24)
+        Me.rdb_byid.Ripple = True
+        Me.rdb_byid.Size = New System.Drawing.Size(61, 30)
         Me.rdb_byid.TabIndex = 3
         Me.rdb_byid.TabStop = True
         Me.rdb_byid.Text = "By ID"
@@ -147,11 +191,17 @@ Partial Class Frm_SelectAccountClient
         'btn_select
         '
         Me.btn_select.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_select.AutoSize = True
+        Me.btn_select.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn_select.Depth = 0
         Me.btn_select.Enabled = False
+        Me.btn_select.Icon = Nothing
         Me.btn_select.Location = New System.Drawing.Point(509, 471)
         Me.btn_select.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_select.MouseState = MaterialSkin.MouseState.HOVER
         Me.btn_select.Name = "btn_select"
-        Me.btn_select.Size = New System.Drawing.Size(112, 35)
+        Me.btn_select.Primary = False
+        Me.btn_select.Size = New System.Drawing.Size(69, 36)
         Me.btn_select.TabIndex = 6
         Me.btn_select.Text = "Select"
         Me.btn_select.UseVisualStyleBackColor = True
@@ -173,16 +223,17 @@ Partial Class Frm_SelectAccountClient
         Me.grp_search.ResumeLayout(False)
         Me.grp_search.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents dgv_accounts As DataGridView
-    Friend WithEvents txt_byid As TextBox
-    Friend WithEvents btn_search As Button
+    Friend WithEvents txt_byid As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents btn_search As MaterialSkin.Controls.MaterialFlatButton
     Friend WithEvents grp_search As GroupBox
-    Friend WithEvents rdb_byname As RadioButton
-    Friend WithEvents txt_byname As TextBox
-    Friend WithEvents rdb_byid As RadioButton
-    Friend WithEvents btn_select As Button
-    Friend WithEvents btn_reset As Button
+    Friend WithEvents rdb_byname As MaterialSkin.Controls.MaterialRadioButton
+    Friend WithEvents txt_byname As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents rdb_byid As MaterialSkin.Controls.MaterialRadioButton
+    Friend WithEvents btn_select As MaterialSkin.Controls.MaterialFlatButton
+    Friend WithEvents btn_reset As MaterialSkin.Controls.MaterialFlatButton
 End Class
--- SafeBoxes_TS2_ALAFAK/Frm_SelectBox.Designer.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_SelectBox.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Frm_SelectBox
-    Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
@@ -22,23 +22,29 @@ Partial Class Frm_SelectBox
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btn_select = New System.Windows.Forms.Button()
+        Me.btn_select = New MaterialSkin.Controls.MaterialFlatButton()
         Me.dgv_boxes = New System.Windows.Forms.DataGridView()
         Me.cbox_boxsizes = New System.Windows.Forms.ComboBox()
         Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
-        Me.btn_search = New System.Windows.Forms.Button()
-        Me.btn_reset = New System.Windows.Forms.Button()
+        Me.btn_search = New MaterialSkin.Controls.MaterialFlatButton()
+        Me.btn_reset = New MaterialSkin.Controls.MaterialFlatButton()
         CType(Me.dgv_boxes, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btn_select
         '
         Me.btn_select.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_select.AutoSize = True
+        Me.btn_select.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn_select.Depth = 0
         Me.btn_select.Enabled = False
-        Me.btn_select.Location = New System.Drawing.Point(530, 296)
+        Me.btn_select.Icon = Nothing
+        Me.btn_select.Location = New System.Drawing.Point(620, 325)
         Me.btn_select.Margin = New System.Windows.Forms.Padding(6, 8, 6, 8)
+        Me.btn_select.MouseState = MaterialSkin.MouseState.HOVER
         Me.btn_select.Name = "btn_select"
-        Me.btn_select.Size = New System.Drawing.Size(168, 54)
+        Me.btn_select.Primary = False
+        Me.btn_select.Size = New System.Drawing.Size(73, 36)
         Me.btn_select.TabIndex = 10
         Me.btn_select.Text = " Select"
         Me.btn_select.UseVisualStyleBackColor = True
@@ -58,7 +64,7 @@ Partial Class Frm_SelectBox
         Me.dgv_boxes.Cursor = System.Windows.Forms.Cursors.Hand
         Me.dgv_boxes.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
         Me.dgv_boxes.GridColor = System.Drawing.Color.PowderBlue
-        Me.dgv_boxes.Location = New System.Drawing.Point(20, 84)
+        Me.dgv_boxes.Location = New System.Drawing.Point(15, 146)
         Me.dgv_boxes.Margin = New System.Windows.Forms.Padding(6, 8, 6, 8)
         Me.dgv_boxes.MultiSelect = False
         Me.dgv_boxes.Name = "dgv_boxes"
@@ -67,7 +73,7 @@ Partial Class Frm_SelectBox
         Me.dgv_boxes.RowHeadersVisible = False
         Me.dgv_boxes.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders
         Me.dgv_boxes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.dgv_boxes.Size = New System.Drawing.Size(678, 196)
+        Me.dgv_boxes.Size = New System.Drawing.Size(678, 163)
         Me.dgv_boxes.TabIndex = 8
         '
         'cbox_boxsizes
@@ -76,7 +82,7 @@ Partial Class Frm_SelectBox
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.cbox_boxsizes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbox_boxsizes.FormattingEnabled = True
-        Me.cbox_boxsizes.Location = New System.Drawing.Point(20, 33)
+        Me.cbox_boxsizes.Location = New System.Drawing.Point(15, 95)
         Me.cbox_boxsizes.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.cbox_boxsizes.Name = "cbox_boxsizes"
         Me.cbox_boxsizes.Size = New System.Drawing.Size(512, 28)
@@ -88,7 +94,7 @@ Partial Class Frm_SelectBox
         Me.MaterialLabel1.Depth = 0
         Me.MaterialLabel1.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel1.Location = New System.Drawing.Point(16, 9)
+        Me.MaterialLabel1.Location = New System.Drawing.Point(11, 71)
         Me.MaterialLabel1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.MaterialLabel1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel1.Name = "MaterialLabel1"
@@ -99,10 +105,16 @@ Partial Class Frm_SelectBox
         'btn_search
         '
         Me.btn_search.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btn_search.Location = New System.Drawing.Point(545, 25)
+        Me.btn_search.AutoSize = True
+        Me.btn_search.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn_search.Depth = 0
+        Me.btn_search.Icon = Nothing
+        Me.btn_search.Location = New System.Drawing.Point(540, 87)
         Me.btn_search.Margin = New System.Windows.Forms.Padding(6, 8, 6, 8)
+        Me.btn_search.MouseState = MaterialSkin.MouseState.HOVER
         Me.btn_search.Name = "btn_search"
-        Me.btn_search.Size = New System.Drawing.Size(105, 43)
+        Me.btn_search.Primary = False
+        Me.btn_search.Size = New System.Drawing.Size(73, 36)
         Me.btn_search.TabIndex = 13
         Me.btn_search.Text = "Search"
         Me.btn_search.UseVisualStyleBackColor = True
@@ -110,11 +122,17 @@ Partial Class Frm_SelectBox
         'btn_reset
         '
         Me.btn_reset.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_reset.AutoSize = True
+        Me.btn_reset.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn_reset.Depth = 0
         Me.btn_reset.Enabled = False
-        Me.btn_reset.Location = New System.Drawing.Point(662, 25)
+        Me.btn_reset.Icon = Nothing
+        Me.btn_reset.Location = New System.Drawing.Point(657, 87)
         Me.btn_reset.Margin = New System.Windows.Forms.Padding(6, 8, 6, 8)
+        Me.btn_reset.MouseState = MaterialSkin.MouseState.HOVER
         Me.btn_reset.Name = "btn_reset"
-        Me.btn_reset.Size = New System.Drawing.Size(36, 43)
+        Me.btn_reset.Primary = False
+        Me.btn_reset.Size = New System.Drawing.Size(30, 36)
         Me.btn_reset.TabIndex = 14
         Me.btn_reset.Text = "X"
         Me.btn_reset.UseVisualStyleBackColor = True
@@ -124,7 +142,7 @@ Partial Class Frm_SelectBox
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(735, 367)
+        Me.ClientSize = New System.Drawing.Size(735, 366)
         Me.Controls.Add(Me.cbox_boxsizes)
         Me.Controls.Add(Me.MaterialLabel1)
         Me.Controls.Add(Me.btn_search)
@@ -141,10 +159,10 @@ Partial Class Frm_SelectBox
 
     End Sub
 
-    Friend WithEvents btn_select As Button
+    Friend WithEvents btn_select As MaterialSkin.Controls.MaterialFlatButton
     Friend WithEvents dgv_boxes As DataGridView
     Friend WithEvents cbox_boxsizes As ComboBox
     Friend WithEvents MaterialLabel1 As MaterialSkin.Controls.MaterialLabel
-    Friend WithEvents btn_search As Button
-    Friend WithEvents btn_reset As Button
+    Friend WithEvents btn_search As MaterialSkin.Controls.MaterialFlatButton
+    Friend WithEvents btn_reset As MaterialSkin.Controls.MaterialFlatButton
 End Class
