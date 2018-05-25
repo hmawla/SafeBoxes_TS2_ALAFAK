bb8e6d99f40ea91e712fff1351f77a1f27c51355
Pedro Marthon <pedro_marthon@live.com>
2018-05-25 21:19:58 +0300

    fixed new client form's design

--- SafeBoxes_TS2_ALAFAK/Frm_NewAccountClient.Designer.vb
+++ SafeBoxes_TS2_ALAFAK/Frm_NewAccountClient.Designer.vb
@@ -22,49 +22,19 @@ Partial Class Frm_NewAccountClient
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.MaterialTabControl1 = New MaterialSkin.Controls.MaterialTabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.txt_cname = New MaterialSkin.Controls.MaterialSingleLineTextField()
         Me.MaterialLabel5 = New MaterialSkin.Controls.MaterialLabel()
         Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
         Me.txt_cid = New MaterialSkin.Controls.MaterialSingleLineTextField()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.MaterialTabSelector1 = New MaterialSkin.Controls.MaterialTabSelector()
-        Me.MaterialTabControl1.SuspendLayout()
-        Me.TabPage1.SuspendLayout()
+        Me.MaterialRaisedButton1 = New MaterialSkin.Controls.MaterialRaisedButton()
         Me.SuspendLayout()
-        '
-        'MaterialTabControl1
-        '
-        Me.MaterialTabControl1.Controls.Add(Me.TabPage1)
-        Me.MaterialTabControl1.Controls.Add(Me.TabPage2)
-        Me.MaterialTabControl1.Depth = 0
-        Me.MaterialTabControl1.Location = New System.Drawing.Point(12, 114)
-        Me.MaterialTabControl1.MouseState = MaterialSkin.MouseState.HOVER
-        Me.MaterialTabControl1.Name = "MaterialTabControl1"
-        Me.MaterialTabControl1.SelectedIndex = 0
-        Me.MaterialTabControl1.Size = New System.Drawing.Size(675, 289)
-        Me.MaterialTabControl1.TabIndex = 0
-        '
-        'TabPage1
-        '
-        Me.TabPage1.BackColor = System.Drawing.Color.White
-        Me.TabPage1.Controls.Add(Me.txt_cname)
-        Me.TabPage1.Controls.Add(Me.MaterialLabel5)
-        Me.TabPage1.Controls.Add(Me.MaterialLabel1)
-        Me.TabPage1.Controls.Add(Me.txt_cid)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 29)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(667, 256)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
         '
         'txt_cname
         '
         Me.txt_cname.Depth = 0
+        Me.txt_cname.Enabled = False
         Me.txt_cname.Hint = ""
-        Me.txt_cname.Location = New System.Drawing.Point(154, 34)
+        Me.txt_cname.Location = New System.Drawing.Point(160, 100)
         Me.txt_cname.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txt_cname.MaxLength = 32767
         Me.txt_cname.MouseState = MaterialSkin.MouseState.HOVER
@@ -74,7 +44,7 @@ Partial Class Frm_NewAccountClient
         Me.txt_cname.SelectionLength = 0
         Me.txt_cname.SelectionStart = 0
         Me.txt_cname.Size = New System.Drawing.Size(212, 23)
-        Me.txt_cname.TabIndex = 60
+        Me.txt_cname.TabIndex = 64
         Me.txt_cname.TabStop = False
         Me.txt_cname.UseSystemPasswordChar = False
         '
@@ -84,12 +54,12 @@ Partial Class Frm_NewAccountClient
         Me.MaterialLabel5.Depth = 0
         Me.MaterialLabel5.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel5.Location = New System.Drawing.Point(3, 10)
+        Me.MaterialLabel5.Location = New System.Drawing.Point(9, 76)
         Me.MaterialLabel5.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.MaterialLabel5.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel5.Name = "MaterialLabel5"
         Me.MaterialLabel5.Size = New System.Drawing.Size(70, 19)
-        Me.MaterialLabel5.TabIndex = 57
+        Me.MaterialLabel5.TabIndex = 62
         Me.MaterialLabel5.Text = "Client ID:"
         '
         'MaterialLabel1
@@ -98,19 +68,19 @@ Partial Class Frm_NewAccountClient
         Me.MaterialLabel1.Depth = 0
         Me.MaterialLabel1.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel1.Location = New System.Drawing.Point(150, 10)
+        Me.MaterialLabel1.Location = New System.Drawing.Point(156, 76)
         Me.MaterialLabel1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.MaterialLabel1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel1.Name = "MaterialLabel1"
         Me.MaterialLabel1.Size = New System.Drawing.Size(96, 19)
-        Me.MaterialLabel1.TabIndex = 59
+        Me.MaterialLabel1.TabIndex = 63
         Me.MaterialLabel1.Text = "Client Name:"
         '
         'txt_cid
         '
         Me.txt_cid.Depth = 0
         Me.txt_cid.Hint = ""
-        Me.txt_cid.Location = New System.Drawing.Point(7, 34)
+        Me.txt_cid.Location = New System.Drawing.Point(13, 100)
         Me.txt_cid.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txt_cid.MaxLength = 32767
         Me.txt_cid.MouseState = MaterialSkin.MouseState.HOVER
@@ -120,59 +90,48 @@ Partial Class Frm_NewAccountClient
         Me.txt_cid.SelectionLength = 0
         Me.txt_cid.SelectionStart = 0
         Me.txt_cid.Size = New System.Drawing.Size(139, 23)
-        Me.txt_cid.TabIndex = 56
+        Me.txt_cid.TabIndex = 61
         Me.txt_cid.TabStop = False
         Me.txt_cid.UseSystemPasswordChar = False
         '
-        'TabPage2
+        'MaterialRaisedButton1
         '
-        Me.TabPage2.BackColor = System.Drawing.Color.White
-        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(667, 256)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        '
-        'MaterialTabSelector1
-        '
-        Me.MaterialTabSelector1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.MaterialTabSelector1.BaseTabControl = Me.MaterialTabControl1
-        Me.MaterialTabSelector1.Depth = 0
-        Me.MaterialTabSelector1.Location = New System.Drawing.Point(-2, 59)
-        Me.MaterialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER
-        Me.MaterialTabSelector1.Name = "MaterialTabSelector1"
-        Me.MaterialTabSelector1.Size = New System.Drawing.Size(704, 37)
-        Me.MaterialTabSelector1.TabIndex = 60
-        Me.MaterialTabSelector1.Text = "MaterialTabSelector1"
+        Me.MaterialRaisedButton1.AutoSize = True
+        Me.MaterialRaisedButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton1.Depth = 0
+        Me.MaterialRaisedButton1.Icon = Nothing
+        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(379, 87)
+        Me.MaterialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton1.Name = "MaterialRaisedButton1"
+        Me.MaterialRaisedButton1.Primary = True
+        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(71, 36)
+        Me.MaterialRaisedButton1.TabIndex = 65
+        Me.MaterialRaisedButton1.Text = "Submit"
+        Me.MaterialRaisedButton1.UseVisualStyleBackColor = True
         '
         'Frm_NewAccountClient
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(699, 415)
-        Me.Controls.Add(Me.MaterialTabSelector1)
-        Me.Controls.Add(Me.MaterialTabControl1)
+        Me.ClientSize = New System.Drawing.Size(464, 135)
+        Me.Controls.Add(Me.MaterialRaisedButton1)
+        Me.Controls.Add(Me.txt_cname)
+        Me.Controls.Add(Me.MaterialLabel5)
+        Me.Controls.Add(Me.MaterialLabel1)
+        Me.Controls.Add(Me.txt_cid)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Frm_NewAccountClient"
-        Me.Text = "New CLient Account"
-        Me.MaterialTabControl1.ResumeLayout(False)
-        Me.TabPage1.ResumeLayout(False)
-        Me.TabPage1.PerformLayout()
+        Me.Text = "New Client's Account"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents MaterialTabControl1 As MaterialSkin.Controls.MaterialTabControl
-    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents txt_cname As MaterialSkin.Controls.MaterialSingleLineTextField
     Friend WithEvents MaterialLabel5 As MaterialSkin.Controls.MaterialLabel
     Friend WithEvents MaterialLabel1 As MaterialSkin.Controls.MaterialLabel
     Friend WithEvents txt_cid As MaterialSkin.Controls.MaterialSingleLineTextField
-    Friend WithEvents TabPage2 As TabPage
-    Friend WithEvents MaterialTabSelector1 As MaterialSkin.Controls.MaterialTabSelector
-    Friend WithEvents txt_cname As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents MaterialRaisedButton1 As MaterialSkin.Controls.MaterialRaisedButton
 End Class
